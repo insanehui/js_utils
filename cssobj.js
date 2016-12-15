adb361e33a7035b66cab130926c3b0550bf2d707
insanehui <269200808@qq.com>
2016-12-15 13:56:42 +0800

    add bg, hsl

--- cssobj.js
+++ cssobj.js
@@ -5,15 +5,36 @@ export function border(color = "black") {
 }
 Object.assign(border, border())
 
-export function flex() {
+export function flex(v) { // 函数用于flex children
   return {
-    display: "flex"
+    flex: v
   }
 }
-Object.assign(flex, flex())
+
+Object.assign(flex, { // 对象用于flex容器
+  display: "flex"
+})
 
 export const ptr = {
   cursor:"pointer",
 }
 
-export default { border, flex, ptr, }
+export function bg(v) {
+  return {
+    background: v,
+  }
+}    
+
+export function hsl(h, s, l, a){
+  // 例：hsl(121, 48, 65) 将可得到 "hsl(121, 48%, 65%)"
+  if( typeof(a) === "undefined" )
+  {
+    return "hsl(" + h + "," + s + "%, " + l + "%)";
+  }
+  else
+  {
+    return "hsla(" + h + "," + s + "%, " + l + "%, " + a + ")";
+  }
+}
+
+export default { border, flex, ptr, bg, hsl, }
