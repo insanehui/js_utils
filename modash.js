4b496295c0d5be66db513787973ae75d4244470e
insanehui <269200808@qq.com>
2016-12-21 21:57:29 +0800

    增加modash

---
+++ modash.js
@@ -0,0 +1,89 @@
+/* 对lodash的一些补充 */
+import _ from 'lodash'
+
+// 内部基础函数
+function _ungroup(o, keys, p){
+  // 这是一个递归函数，之前在cdb团队写过c++版的json_ungroup，后来代码遗失，今天重新梳理 2016年12月21日
+  // 这里o是一个对象，keys为还剩下需要ungroup的序列，为数组，p为已经积累的需要合并的数据
+
+  if ( !_.isObject(o) ) {
+    return [] // 始终返回为一个数组，行为统一，便于递归
+  }
+
+  // 已经结束
+  if ( !_.size(keys) ) {
+    return [{...o, ...p}] 
+  } 
+
+  // 进入递归
+  // 取出keys第一个元素
+  const [key, ...rest] = keys
+  let arr = []
+  _.each(o, (v, k ) => {
+    arr = [...arr, ..._ungroup(v, rest, {...p, [key]:k})]
+  })
+
+  return arr
+}
+
+// 导出函数
+export function ungroup(o, ...keys){
+  return _ungroup(o, keys, {})
+}
+
+export function test(){
+  console.log('test!!', [1, ...null])
+}
+
+
+export function test_ungroup(){
+  // 原始数据
+  const from = {
+    a : {
+      a1 : { 
+        val : 1 
+      }, 
+      a2 : { 
+        val : 2,
+      }, 
+    },
+    b : {
+      b1 : { 
+        val : 3,
+      }, 
+      b2 : { 
+        val : 4,
+      }, 
+    },
+  }
+
+  // 最终期望替换成
+  const to = [
+      { 
+        key1: 'a',
+        key2: 'a1',
+        val : 1,
+      }, 
+      { 
+        key1: 'a',
+        key2: 'a2',
+        val : 2,
+      }, 
+      { 
+        key1: 'b',
+        key2: 'b1',
+        val : 3,
+      }, 
+      { 
+        key1: 'b',
+        key2: 'b2',
+        val : 4,
+      }, 
+  ]
+
+  const r = ungroup(from, 'key1', 'key2')
+  console.log(_.isEqual(r, to) )
+
+}
+
+
