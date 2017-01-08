d0fce2aefc26d3ae17136cb62a98893f91748a02
insanehui <269200808@qq.com>
2017-01-08 22:38:50 +0800

    增加fetch api

---
+++ fetch.js
@@ -0,0 +1,15 @@
+// fetch api的一些小封装
+import form_encode from 'form-urlencoded'
+
+// post, 返回promise
+export function post(url, para) {
+  const headers = {
+    'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+  }
+
+  return fetch(url, {
+    method : 'POST', headers, body : form_encode(para),
+  }).then(res => res.json())
+}
+
