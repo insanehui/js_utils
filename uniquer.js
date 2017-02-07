85a4348c9cdaf0b2c312f9b691ded8b4e48d8ff1
insanehui <269200808@qq.com>
2017-02-07 15:08:44 +0800

    增加uadd

--- uniquer.js
+++ uniquer.js
@@ -1,11 +1,12 @@
 // 用于生成uuid，支持用户传入候选值
 
 // 引用计数器
+import _ from 'lodash'
 import {_count, scount} from './counter.js'
 
 let s = new Set([undefined]) // 用于下文判断方便，预存一个undefined
 
-export function uniquer(val){
+export function gen(val){
   while ( s.has(val) ) {
     if ( val === undefined ) {
       val = scount()
@@ -15,4 +16,15 @@ export function uniquer(val){
   } 
   s.add(val)
   return val
+}
+
+// 常用于将集合初始化的场合
+export function add(val){
+  if ( _.isArray(val) ) {
+    for(let i of val){
+      s.add(i)
+    }
+  } else {
+    s.add(val)
+  }
 }
