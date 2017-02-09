2bf5a2745a6ca0e4938d06b3d42199ff0d202a99
insanehui <269200808@qq.com>
2017-02-09 15:51:27 +0800

    准备用Promise写一个jsonGet

--- node/http.js
+++ node/http.js
@@ -1,3 +1,4 @@
+// http小工具
 
 // 网上摘抄的获取客户端ip的写法
 export function client_ip(req) {
@@ -11,5 +12,44 @@ export function client_port(req) {
   return req.connection.remotePort ||
     req.socket.remotePort ||
     req.connection.socket.remotePort;
+}
+
+// 简易的返回json的方法
+export function write_json(res, data) {
+  res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'}) // 历史原因，有些代码里用的是plain，但后续还是统一用json规范一些
+  const ret = JSON.stringify(data)
+  console.log(">>>>>>>>>>>>>>>>>>>>>>", ret)
+  res.end(ret);
+}
+
+// 基于npm httpdispatcher 封装的一个简易的router
+export class Router {
+
+  constructor(dispatcher) {
+    this.dispatcher = dispatcher
+  }
+  
+  onGet(path, handler) { // handler(qs, req, res)
+    this.dispatcher.onGet(path, function(req, res){
+      try {
+
+        const url = require('url')
+
+        // 取到参数
+        let qs = url.parse(req.url, true).query;
+        console.log("qs", qs)
+
+        handler(qs, req, res)
+
+        // res.writeHead(200, {'Content-Type': 'text/plain'});
+        // res.end(ret);
+
+      } catch(e) {
+        console.log("err", e)
+        res.writeHead(500)
+      }
+    })
+  }
+
 }
 
