baf94fa735664dfdcd99f4df0d9b4da17cbe0343
insanehui <269200808@qq.com>
2017-02-15 18:55:42 +0800

    从浏览器端拷贝了一个fetch方法，但好像不能用

--- node/http.js
+++ node/http.js
@@ -1,4 +1,6 @@
 // http小工具
+import form_encode from 'form-urlencoded'
+import fetch from 'node-fetch'
 
 // 网上摘抄的获取客户端ip的写法
 export function client_ip(req) {
@@ -17,9 +19,22 @@ export function client_port(req) {
 // 简易的返回json的方法
 export function write_json(res, data) {
   res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'}) // 历史原因，有些代码里用的是plain，但后续还是统一用json规范一些
-  const ret = JSON.stringify(data)
+  const ret = JSON.stringify(data, null, '  ')
   console.log(">>>>>>>>>>>>>>>>>>>>>>", ret)
   res.end(ret);
+}
+
+// post, 返回promise
+export function post(url, para) {
+
+  const headers = {
+    'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+  }
+
+  return fetch(url, {
+    method : 'POST', headers, body : form_encode(para),
+  }).then(res => res.json())
 }
 
 // 基于npm httpdispatcher 封装的一个简易的router
