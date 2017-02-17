ca4c9c898cbbb4f0f78fe22739825395a10a8373
insanehui <269200808@qq.com>
2017-02-17 14:31:54 +0800

    删除落单函数，暂未测试

--- uniquer.js
+++ uniquer.js
@@ -4,32 +4,6 @@
 import _ from 'lodash'
 import {_count, scount} from './counter.js'
 
-// TODO: 既然已经定义了类，将这些逻辑重复的零散函数干掉
-let s = new Set([undefined]) // 用于下文判断方便，预存一个undefined
-
-export function gen(val){
-  while ( s.has(val) ) {
-    if ( val === undefined ) {
-      val = scount()
-    } else {
-      val += _count()
-    }
-  } 
-  s.add(val)
-  return val
-}
-
-// 常用于将集合初始化的场合
-export function add(val){
-  if ( _.isArray(val) ) {
-    for(let i of val){
-      s.add(i)
-    }
-  } else {
-    s.add(val)
-  }
-}
-
 class Uniquer {
 
   s = new Set([undefined])
@@ -58,6 +32,12 @@ class Uniquer {
     }
   }
 }
+
+const def = new Uniquer()
+
+export const add = def.add.bind(def)
+
+export const gen = def.gen.bind(def)
 
 export function New(){
   return new Uniquer()
