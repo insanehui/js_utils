7b2bf22765cf600f43fb8a82dd310a6e088656b1
insanehui <269200808@qq.com>
2017-02-17 14:28:37 +0800

    导出了类，但是未测试

--- uniquer.js
+++ uniquer.js
@@ -4,6 +4,7 @@
 import _ from 'lodash'
 import {_count, scount} from './counter.js'
 
+// TODO: 既然已经定义了类，将这些逻辑重复的零散函数干掉
 let s = new Set([undefined]) // 用于下文判断方便，预存一个undefined
 
 export function gen(val){
@@ -27,4 +28,37 @@ export function add(val){
   } else {
     s.add(val)
   }
+}
+
+class Uniquer {
+
+  s = new Set([undefined])
+  
+  gen(val) {
+    const s = this.s
+    while ( s.has(val) ) {
+      if ( val === undefined ) {
+        val = scount()
+      } else {
+        val += _count()
+      }
+    } 
+    s.add(val)
+    return val
+  }
+
+  add(val) {
+    const s = this.s
+    if ( _.isArray(val) ) {
+      for(let i of val){
+        s.add(i)
+      }
+    } else {
+      s.add(val)
+    }
+  }
+}
+
+export function New(){
+  return new Uniquer()
 }
