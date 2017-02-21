d0761c515a8b2b52b74ba89a31b673412f459152
insanehui <269200808@qq.com>
2017-02-21 14:32:08 +0800

    初步写了一点样式

--- components/Input.js
+++ components/Input.js
@@ -29,18 +29,41 @@ export class InputCore extends PureComponent {
 
     const st = {
       outline: "none", // 没有边框
-      borderStyle: "none", // 没有border
-      backgroundColor: "transparent", // 背景透明
+      border: "none", // 没有border
+      background: "transparent", // 背景透明
 
       // 便于嵌到其他元素里，以形成自定义的input样式
     }
 
     return <input {...s} 
       value={s.value || ''}
-      style={{...s.style, ...st}}
+      style={{...st, ...s.style, }}
       onChange={this.onChange.bind(this)}
     />
   }
 }
 
+export const input_style_github = {
+  color: '#333',
+  verticalAlign: 'middle',
+  backgroundColor: '#fff',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'right 8px center',
+  border: '1px solid #ddd',
+  borderRadius: '3px',
+  outline: 'none',
+  boxShadow: 'inset 0 1px 2px rgba(0,0,0,0.075)',
+  padding : '3px 8px',
+  fontSize: '12px',
+  lineHeight: '20px',
+}
+
+export function GitHub(p){ // github style
+  const st = {
+    ...input_style_github,
+    ...p.style,
+  }
+
+  return <InputCore {...p} style={st} />
+}
 
