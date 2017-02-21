ead7b9a761ca5c75349921181477e05d1e10b766
insanehui <269200808@qq.com>
2017-02-21 17:07:19 +0800

    引入styles规整一下样式

--- components/ThemeGitHub.js
+++ components/ThemeGitHub.js
@@ -7,27 +7,29 @@ import {InputCore} from './Input.js'
 import ActiveStyle from './ActiveStyle.js'
 import {merge_props as merge} from './utils.js'
 
-export const input_style_github = {
-  color: '#333', // 字体颜色
-  verticalAlign: 'middle',
-  background: 'right 8px center no-repeat rgb(255, 255, 255)',
-  border: '1px solid', // 注：这里有一个React处理style时的bug, 如果把border相关的合并到一个属性的话，直接展示normal 和 focus -> normal，得到的最终html样式会有区别
-  borderColor: '#ddd', 
-  borderRadius: '3px',
-  outline: 'none',
-  boxShadow: 'inset 0 1px 2px rgba(0,0,0,0.075)',
-  padding : '3px 8px',
-  fontSize: '12px',
-  lineHeight: '20px',
-  '&:focus' : {
-    borderColor: '#51a7e8', 
-    boxShadow: 'inset 0 1px 2px rgba(0,0,0,0.075), 0 0 5px rgba(81,167,232,0.5)',
-  },
+const styles = {
+  input : {
+    color: '#333', // 字体颜色
+    verticalAlign: 'middle',
+    background: 'right 8px center no-repeat rgb(255, 255, 255)',
+    border: '1px solid', // 注：这里有一个React处理style时的bug, 如果把border相关的合并到一个属性的话，直接展示normal 和 focus -> normal，得到的最终html样式会有区别
+    borderColor: '#ddd', 
+    borderRadius: '3px',
+    outline: 'none',
+    boxShadow: 'inset 0 1px 2px rgba(0,0,0,0.075)',
+    padding : '3px 8px',
+    fontSize: '12px',
+    lineHeight: '20px',
+    '&:focus' : {
+      borderColor: '#51a7e8', 
+      boxShadow: 'inset 0 1px 2px rgba(0,0,0,0.075), 0 0 5px rgba(81,167,232,0.5)',
+    },
+  }, 
 }
 
 export function Input(p){ 
 
-  return <ActiveStyle {...merge(p, input_style_github)} >
+  return <ActiveStyle {...merge(p, styles.input)} >
     <InputCore/>
   </ActiveStyle>
 }
