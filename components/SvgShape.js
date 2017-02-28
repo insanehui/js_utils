321a3a87f9bc47f742d2c59accdddea42e4414b1
insanehui <269200808@qq.com>
2017-02-28 15:12:56 +0800

    箭头支持旋转

--- components/SvgShape.js
+++ components/SvgShape.js
@@ -4,26 +4,35 @@ import React from 'react'
 
 export function Arrow(p){
 
-  // return <path d="M 1484.88 593 L 1477.88 596.5 L 1479.63 593 L 1477.88 589.5 Z" fill="#000000" stroke="#000000" stroke-miterlimit="10" pointer-events="all"></path>
   // 简化的箭头点如下：
   // 0
-  //       1
+  //       1(原点0,0)
   // 2
-  const p0 = { x: 0, y : 0, }
-  const p1 = { x: 200, y : 100, }
-  const p2 = { x: 0, y : 200, }
+  const p0 = { x: -200, y : 100, }
+  const p1 = { x: 0, y : 0, }
+  const p2 = { x: -200, y : -100, }
   const w0 = p1.x - p0.x
   const h0 = p2.y - p0.y
 
-  const {width, height} = p
+  let {w, h, x, y, tx, ty} = p
+  x -= 0
+  y -= 0
+  w -= 0
+  h -= 0
+  tx -= 0
+  ty -= 0
 
-  const transform = `scale(${width/w0},${height/h0})`
+  const transform = `translate(${x}, ${y}) scale(${w/w0},${h/h0}) rotate(${180*Math.atan2(ty, tx)/Math.PI})`
 
   return <path d={`M ${p0.x} ${p0.y} L ${p1.x} ${p1.y} L ${p2.x} ${p2.y} Z`} transform={transform} fill="#000000" stroke="#000000" strokeMiterlimit="10" pointerEvents="all"></path>
-  // return <path d="M10 80 Q 95 10 180 80" stroke="black" fill="transparent" />
 }
 
 Arrow.defaultProps = {
-  width:7,
-  height:5,
+  // 缺省状态下，是一个200 x 200 的箭头图案
+  x : 200, 
+  y : 100, 
+  w : 200,
+  h : 200,
+  tx : 1, // 表示actangent的角度
+  ty : 0,
 }
