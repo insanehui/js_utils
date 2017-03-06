6ff7483e252bdc8fd923ca1b28cf14438a3fe7bf
insanehui <269200808@qq.com>
2017-03-07 00:19:47 +0800

    增加uniquer的用例

--- uniquer.test.js
+++ uniquer.test.js
@@ -1,4 +1,23 @@
 import {New} from './uniquer.js'
+import _ from 'lodash'
+
+test('uniquer', () => {
+  const tb = [
+    ['a', 'a'],
+    ['a', 'a1'],
+    ['a', 'a2'],
+  ]
+
+  const u = New('', 1)
+
+  for (let i of tb) {
+    const para = _.initial(i)
+    const hope = _.last(i)
+    const fact = u(...para)
+
+    expect(fact).toBe(hope)
+  }
+})
 
 test('uniquer.gen', () => {
   const u = New()
