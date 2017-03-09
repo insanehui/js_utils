56a8cb30f448dcb4c191b9dbe2f3df54954b1944
insanehui <269200808@qq.com>
2017-03-09 15:21:58 +0800

    增加addStyle。hoc的生成函数

--- components/utils.js
+++ components/utils.js
@@ -1,6 +1,6 @@
 // 提供一些常用的React组件编写的工具
 import _ from 'lodash'
-import { PureComponent } from 'react'
+import React, { PureComponent } from 'react'
 
 export function merge_props(p0, p1){ // 两个props合并，主要是针对style再合并一层
 
@@ -43,7 +43,7 @@ export function merge_props_with_def_style(def_style, p){ // 返回新的props
   }
 }
 
-export class PS extends PureComponent {
+export class PS extends PureComponent { // 注：ps是一个很有争议的模式，慎用！使用时多思考用React标准的controlled和uncontrolled方式是否会更好
   constructor(p) {
     super(p)
     
@@ -55,11 +55,25 @@ export class PS extends PureComponent {
   }
 }
 
+export const addStyle = st=> Cmp => { // => fn(Cmp0) => Cmp1，模仿redux里高阶组件的写法，使用柯里化
+  
+  class styled extends PureComponent {
+
+    render() {
+      const p = this.props 
+      return <Cmp {...merge_props_with_def_style(st, p)} />
+    }
+  }
+
+  return styled
+}
+
 export function get_xy(event, el){ // 根据event（通常是鼠标事件），计算出其相对于html元素的client坐标
   const {top, left} = el.getBoundingClientRect()
   const x = event.clientX - left
   const y = event.clientY - top
   return {x, y}
 }
+
 
 
