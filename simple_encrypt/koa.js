96c53d16afe55e3b97f8e5c953aff51cb33ddfdc
insanehui <269200808@qq.com>
2017-03-21 18:07:51 +0800

    初步写好koa的中间件，暂未测试

---
+++ simple_encrypt/koa.js
@@ -0,0 +1,22 @@
+/*
+ * 基于simple_encrypt的一个koa中间件
+ */
+import {encode} from './encrypt.js'
+import uuid from 'uuid/v1' 
+
+export const req_xkey = 'X-WarCraft'
+export const req_xval = 'greedisgood'
+
+export const res_xkey = 'X-CloudToGo-Web' // 返回的头，表示已加密。值可以为任意值，仅用来混淆视听:)
+
+export async function simple_encrypt(ctx, next) {
+
+  if ( ctx.header[req_xkey] !== req_xval ) { // 当请求的http头包含了暗号，则不加密
+    // 采用加密逻辑
+    // 设置加密头
+    ctx.set(res_xkey, uuid().replace('-',''))
+    ctx.body = encode(ctx.body)
+  } 
+
+  await next()
+}
