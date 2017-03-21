1d6e020edddec598d7c7de531b022f7bc449bebc
insanehui <269200808@qq.com>
2017-03-21 21:54:50 +0800

    测试svr增加了初始化和清理

--- simple_encrypt/client_koa.test.js
+++ simple_encrypt/client_koa.test.js
@@ -4,12 +4,18 @@ import http from 'http'
 import Koa from 'koa'
 
 const app = new Koa()
-
 app.use(ctx => {
   ctx.body = '{"name" : "tcli", "age" : 18}' // 一个http服务器，任何请求返回haha
 })
+let server
 
-const server = app.listen(666)
+beforeAll(() => {
+  server = app.listen(666)
+})
+
+afterAll(() => {
+  server && server.close()
+})
 
 test('learn_supertest', async () => {
   const txt = await get('http://localhost:666')
