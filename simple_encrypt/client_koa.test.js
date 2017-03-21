833d9ff26afef13c689cc4aceea90b043ac6ba69
insanehui <269200808@qq.com>
2017-03-21 19:48:14 +0800

    临时写了一些测试代码

---
+++ simple_encrypt/client_koa.test.js
@@ -0,0 +1,18 @@
+import {get} from './client.js'
+import http from 'http'
+
+import Koa from 'koa'
+
+const app = new Koa()
+
+app.use(ctx => {
+  ctx.body = '{"name" : "tcli", "age" : 18}' // 一个http服务器，任何请求返回haha
+})
+
+const server = app.listen(666)
+
+test('learn_supertest', async () => {
+  const txt = await get('http://localhost:666')
+  console.log(txt)
+})
+
