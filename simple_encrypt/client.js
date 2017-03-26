814df4d6d6d99cacc886d1ca615b4475091dda8e
insanehui <269200808@qq.com>
2017-03-26 16:43:03 +0800

    删除一deprecated函数，避免在前端的环境引用node-fetch,造成大量编译警告

--- simple_encrypt/client.js
+++ simple_encrypt/client.js
@@ -1,24 +1,24 @@
 /*
  * koa插件配套的客户端方法
  */
-import form_encode from 'form-urlencoded'
 import {decode} from './encrypt.js'
 
-export async function get(url, para) {
-  const full_url = url + '?' + form_encode(para)
+// import form_encode from 'form-urlencoded'
+// export async function get(url, para) {
+//   const full_url = url + '?' + form_encode(para)
 
-  let req = fetch
-  if( process ){
-    req = require('node-fetch')
-  }
+//   let req = fetch
+//   if( process ){
+//     req = require('node-fetch')
+//   }
 
-  const xtxt = await req(full_url, {
-    method : 'GET', 
-  }).then(res => res.text())
+//   const xtxt = await req(full_url, {
+//     method : 'GET', 
+//   }).then(res => res.text())
 
-  const txt = decode(xtxt)
-  return JSON.parse(txt)
-}
+//   const txt = decode(xtxt)
+//   return JSON.parse(txt)
+// }
 
 export async function ketch_(res){ // fetchoa后件
   const xtxt = await res.text()
