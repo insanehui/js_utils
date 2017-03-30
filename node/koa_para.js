213775bfef2046772cc86e4614c4f66f0f36c6df
insanehui <269200808@qq.com>
2017-03-30 14:35:26 +0800

    first add koa_para

---
+++ node/koa_para.js
@@ -0,0 +1,23 @@
+/*
+ * 将koa-body解析出来的结果与ctx.query对象，进行合并
+ * 从而实现统一处理post和get（query string）请求里的参数
+ */
+import koaBody from 'koa-body'
+
+const koa_body = koaBody()
+
+export async function para(ctx, next) {
+
+  ctx.params = ctx.query
+
+  /*
+   * 注意这里对接中间件的方式：
+   * 将koa-body的next参数传入一个自写的函数
+   * 在该函数里使用其处理的结果，并且接上原本的next
+   */
+  await koa_body(ctx, async x=>{
+    ctx.params = { ...ctx.params, ...ctx.request.body }
+    await next()
+  })
+}
+
