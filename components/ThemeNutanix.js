9a95e03f9cb98f1fa42fc5649cba244b987b0c80
insanehui <269200808@qq.com>
2017-04-08 00:31:57 +0800

    初步增加nutanix的style

---
+++ components/ThemeNutanix.js
@@ -0,0 +1,17 @@
+/*
+ * nutanix主题控件
+ */
+// import React from 'react'
+
+// import _ from 'lodash'
+
+import {addStyle} from './utils.js'
+// import {Input as InputBase} from './Form.js'
+// import {_active, Active, } from './ActiveStyle.js'
+// import {merge_props_with_def_style as merge_st, merge_props, PS} from './utils.js'
+
+import {bg, hsl, } from '../cssobj.js'
+
+export const Header = addStyle({
+  ...bg( hsl(220, 3, 21) ),
+})('div')
