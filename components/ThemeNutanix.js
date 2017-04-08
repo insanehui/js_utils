c34f818c461f0582997748e07496f78127fcdc02
insanehui <269200808@qq.com>
2017-04-08 17:31:05 +0800

    增加一个preset

--- components/ThemeNutanix.js
+++ components/ThemeNutanix.js
@@ -10,8 +10,19 @@ import {addStyle} from './utils.js'
 // import {_active, Active, } from './ActiveStyle.js'
 // import {merge_props_with_def_style as merge_st, merge_props, PS} from './utils.js'
 
-import {bg, hsl, } from '../cssobj.js'
+import {css, bg, hsl, } from '../cssobj.js'
 
 export const Header = addStyle({
   ...bg( hsl(220, 3, 21) ),
 })('div')
+
+export function preset(){
+  css({
+    '@global' : {
+      body : {
+        ...bg(hsl(206, 24, 94))
+      }, 
+    },
+  })
+}
+
