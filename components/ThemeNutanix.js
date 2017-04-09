02bf57e8176e100223e6844cdbf565ff0045f7c6
insanehui <269200808@qq.com>
2017-04-09 11:52:47 +0800

    尝试封装Label和Labelet

--- components/ThemeNutanix.js
+++ components/ThemeNutanix.js
@@ -10,7 +10,7 @@ import {addStyle} from './utils.js'
 // import {_active, Active, } from './ActiveStyle.js'
 // import {merge_props_with_def_style as merge_st, merge_props, PS} from './utils.js'
 
-import {css, bg, hsl, } from '../cssobj.js'
+import {css, bg, hsl, inblock, } from '../cssobj.js'
 
 export const Header = addStyle({
   ...bg( hsl(220, 3, 21) ),
@@ -20,9 +20,19 @@ export function preset(){
   css({
     '@global' : {
       body : {
-        ...bg(hsl(206, 24, 94))
+        ...bg(hsl(206, 24, 94)),
       }, 
     },
   })
 }
+
+export const Label = addStyle({
+  fontSize: 14,
+  color: hsl(196, 7, 57),
+  fontWeight: 600,
+  lineHeight: 1.5,
+  padding: '4px 5px',
+})('div')
+
+export const Labelet = addStyle(inblock)(Label)
 
