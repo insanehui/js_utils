5f52904a0094c105f1319397a83cc19375fa4764
insanehui <269200808@qq.com>
2017-04-09 13:49:00 +0800

    export font

--- components/ThemeNutanix.js
+++ components/ThemeNutanix.js
@@ -1,24 +1,25 @@
 /*
  * nutanix主题控件
  */
-// import React from 'react'
-
-// import _ from 'lodash'
-
+import _ from 'lodash'
 import {addStyle} from './utils.js'
-// import {Input as InputBase} from './Form.js'
-// import {_active, Active, } from './ActiveStyle.js'
-// import {merge_props_with_def_style as merge_st, merge_props, PS} from './utils.js'
 
 import {css, bg, hsl, inblock, } from '../cssobj.js'
 
-export const Header = addStyle({
-  ...bg( hsl(220, 3, 21) ),
-})('div')
+export const S = (x=>{
+  const hue = 206
 
-export const S = {
-  barbg : bg(hsl(206, 24, 94)),
-}
+  return {
+    barbg : bg(hsl(hue, 24, 94)), // 淡灰绿色的背景，作为ui的底框
+    font : {
+      dark : {
+        // color: hsl(hue, 9, 28), // 深色的字体
+        color: `rgb(66, 73, 81)`, // 深色的字体
+        fontWeight: 600, 
+      }
+    }
+  }
+})()
 
 export function preset(){
   css({
@@ -29,6 +30,10 @@ export function preset(){
     },
   })
 }
+
+export const Header = addStyle({
+  ...bg( hsl(220, 3, 21) ),
+})('div')
 
 export const Label = addStyle({
   fontSize: 14,
@@ -47,3 +52,7 @@ export const box = addStyle({
 })
 
 export const Box = box('div')
+
+export const font = _.curry((theme, cmp)=>{
+  return addStyle(S.font[theme])(cmp)
+})
