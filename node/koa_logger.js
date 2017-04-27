d65d4fd1c78a1c9046ae63755c4da4160040c261
insanehui <269200808@qq.com>
2017-04-27 16:39:49 +0800

    丰富koa_logger的日志信息

--- node/koa_logger.js
+++ node/koa_logger.js
@@ -1,10 +1,13 @@
 /*
  * koa一个打日志的小中间件
  */
-const debug = require('debug')('koa_logger')
+
+const debug = require('debug')('utils:koa_logger')
+const log_header = require('debug')('utils:koa_logger:header')
 
 export async function logger(ctx, next) { 
-  debug(`${ctx.method} ${ctx.url}`);
+  debug(`${ctx.method} ${ctx.url}`)
+  log_header(ctx.headers)
   await next();
 }
 
