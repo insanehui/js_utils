665b582ce202af080522182f7eead86043a9718f
insanehui <269200808@qq.com>
2017-05-02 16:52:06 +0800

    增加新的proxy2

--- simple_encrypt/koa.js
+++ simple_encrypt/koa.js
@@ -9,6 +9,8 @@ export const req_xval = 'greedisgood'
 
 export const res_xkey = 'x-cloudtogo-web' // 返回的头，表示已加密。值可以为任意值，仅用来混淆视听:)
 
+const log = require('debug')('utils:simple_encrypt')
+
 export async function simple_encrypt(ctx, next) {
 
   if ( ctx.header[req_xkey] !== req_xval ) { // 当请求的http头包含了暗号，则不加密
@@ -19,4 +21,18 @@ export async function simple_encrypt(ctx, next) {
   } 
 
   await next()
+}
+
+/*
+ * 于2017年5月2日重写simple_encrypt: 主要将加密的内容放在尾端
+ */
+export async function koa(ctx, next){
+  await next() 
+
+  if ( ctx.header[req_xkey] !== req_xval ) { // 仅当请求的http头未包含了暗号，则加密
+    // 设置加密头
+    ctx.set(res_xkey, uuid().replace(/-/g,''))
+    ctx.body = encode(ctx.body)
+    log('set body encrypted')
+  }
 }
