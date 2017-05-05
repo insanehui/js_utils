a584ee5f00abd02bb8e617d55cf910e76061befa
insanehui <269200808@qq.com>
2017-05-05 17:49:40 +0800

    完全注释掉proxy，优化proxy2里可能存在的问题

--- node/koa_proxy.js
+++ node/koa_proxy.js
@@ -12,67 +12,70 @@ import _ from 'lodash'
 const log = require('debug')('utils:koa_proxy');
 const debug = require('debug')('debug:utils:koa_proxy');
 
-export const proxy = url => async ctx=>{ // 注意该中间件没有next，是一个封口件
+// export const proxy = url => async ctx=>{ // 注意该中间件没有next，是一个封口件
+
+//   debug("req header", JSON.stringify(ctx.headers, null, '  '))
+
+//   const dest_url = Url.parse(url)
+
+//   var opt = { 
+
+//     // hostname: 'mart.cloudtogo.cn',
+//     // path: '/app/list',
+//     // port : 80,
+
+//     // hostname: 'localhost',
+//     // path: '/bar',
+//     // port : 8084,
+
+//     // hostname: 'composer.dev.cloudtogo.cn',
+//     // path: '/get_blueprint',
+//     // port : 8084,
+
+//     ...dest_url,
+//     path : dest_url.path + (ctx.querystring ? '?' + ctx.querystring : ''),
+//     method: ctx.method,
+//     headers : _.omit(ctx.headers, 'accept-encoding', 'host', ),
+//   }
+
+//   log(`-> ${url}`)
+//   debug('req opt: ', opt)
+
+//   const req = http.request(opt, (res) => {
+//     log(`<- ${url} res code: ${res.statusCode}`)
+//     debug(`res header: ${JSON.stringify(res.headers)}`)
+
+//     // 透传返回码以及headers
+//     ctx.status = res.statusCode
+//     ctx.set(res.headers)
+//   })
+
+//   req.on('response', (response) => {
+//     response.pipe(ctx.res)
+//   })
+
+//   ctx.req.pipe(req)
+
+//   // req和res都要promise化才能正常工作
+//   await streamToPromise(ctx.req) // 这里传入req也是可以的
+//   await streamToPromise(ctx.res) 
+
+// }
+
+export const proxy2 = url => async (ctx, next)=>{ // 不封口的版本
 
   debug("req header", JSON.stringify(ctx.headers, null, '  '))
 
   const dest_url = Url.parse(url)
 
   var opt = { 
-
-    // hostname: 'mart.cloudtogo.cn',
-    // path: '/app/list',
-    // port : 80,
-
-    // hostname: 'localhost',
-    // path: '/bar',
-    // port : 8084,
-
-    // hostname: 'composer.dev.cloudtogo.cn',
-    // path: '/get_blueprint',
-    // port : 8084,
-
     ...dest_url,
     path : dest_url.path + (ctx.querystring ? '?' + ctx.querystring : ''),
     method: ctx.method,
-    headers : _.omit(ctx.headers, 'accept-encoding', 'host', ),
-  }
-
-  log(`-> ${url}`)
-  debug('req opt: ', opt)
-
-  const req = http.request(opt, (res) => {
-    log(`<- ${url} res code: ${res.statusCode}`)
-    debug(`res header: ${JSON.stringify(res.headers)}`)
-
-    // 透传返回码以及headers
-    ctx.status = res.statusCode
-    ctx.set(res.headers)
-  })
-
-  req.on('response', (response) => {
-    response.pipe(ctx.res)
-  })
-
-  ctx.req.pipe(req)
-
-  // req和res都要promise化才能正常工作
-  await streamToPromise(ctx.req) // 这里传入req也是可以的
-  await streamToPromise(ctx.res) 
-
-}
-
-export const proxy2 = url => async (ctx, next)=>{ // 不封口的版本
-
-  debug("req header", JSON.stringify(ctx.headers))
-
-  const dest_url = Url.parse(url)
-
-  var opt = { 
-    ...dest_url,
-    path : dest_url.path + (ctx.querystring ? '?' + ctx.querystring : ''),
-    method: ctx.method,
-    headers : _.omit(ctx.headers, 'accept-encoding', 'host', ),
+    /*
+     * 去掉accept：因为小郎的服务器不支持传统的浏览器默认添加的accept，故去掉
+     */
+    headers : _.omit(ctx.headers, 'accept-encoding', 'host', 'accept'),
   }
 
   log(`-> ${url}`)
@@ -82,11 +85,14 @@ export const proxy2 = url => async (ctx, next)=>{ // 不封口的版本
 
   const req = http.request(opt, (res) => {
     log(`<- ${url} res code: ${res.statusCode}`)
-    debug(`res header: ${JSON.stringify(res.headers)}`)
+    debug(`res header: ${JSON.stringify(res.headers, null, '  ')}`)
 
     // 透传返回码以及headers
     ctx.status = res.statusCode
-    ctx.set(_.omit(res.headers, 'content-length'))
+    /*
+     * content-length和transfer-encoding都要去掉，否则两者会打架
+     */
+    ctx.set(_.omit(res.headers, 'content-length', 'transfer-encoding'))
   })
 
   ctx.req.pipe(req)
