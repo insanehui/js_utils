a955f7245f02188333f60ea3408e534f82e57e74
insanehui <269200808@qq.com>
2017-05-09 00:27:24 +0800

    koa_logger合并koa_catch的功能

--- node/koa_logger.js
+++ node/koa_logger.js
@@ -1,21 +1,39 @@
 /*
  * koa一个打日志的小中间件
+ * 完全包含koa_catch的功能
  */
 
+import 'colors'
 const log = require('debug')('utils:koa_logger')
 const debug = require('debug')('debug:utils:koa_logger')
+
 import {count} from '../counter.js' // 计数器，用来统计会话
+import {tostr} from '../modash.js'
 
 export async function logger(ctx, next) { 
-  // 给会话编个号
   const id = ctx.state.id = count()
 
-  log(`[${id}] <<<<<<<<<<<<< ${(ctx.method + ' ' + ctx.url).green} ${ctx.request.ip} <<<<<<<<<<<<<`)
-  debug(ctx.headers)
+  try {
+    // 给会话编个号
 
-  await next()
+    log(`[${id}] <<<<<<<<<<<<< ${(ctx.method + ' ' + ctx.url).green} ${ctx.request.ip} <<<<<<<<<<<<<`)
+    debug(ctx.headers)
 
-  log(`[${id}] >>>>>>>>>>>>> ${(ctx.status + '')[ctx.status === 200 ? 'green' : 'red']} >>>>>>>>>>>>>`)
-  debug(ctx.body)
+    await next()
+
+  } catch(err) {
+    ctx.status = 500
+    ctx.body = err
+  }
+
+  const ok = ctx.status === 200
+
+  log(`[${id}] >>>>>>>>>>>>> ${(ctx.status + '')[ok ? 'green' : 'red']} >>>>>>>>>>>>>`)
+
+  if ( ok ) {
+    debug(ctx.body)
+  } else {
+    console.error(`  ${`${'ERROR'} [${id}] ${tostr(ctx.body)}`.white.bgRed}`)
+  }
 }
 
