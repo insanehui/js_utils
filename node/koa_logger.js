eaae9a533d18fbd1b73686c7c1dd84844a774be6
insanehui <269200808@qq.com>
2017-05-09 00:15:41 +0800

    增加给会话编序号

--- node/koa_logger.js
+++ node/koa_logger.js
@@ -4,14 +4,18 @@
 
 const log = require('debug')('utils:koa_logger')
 const debug = require('debug')('debug:utils:koa_logger')
+import {count} from '../counter.js' // 计数器，用来统计会话
 
 export async function logger(ctx, next) { 
-  log(`<<<<<<<<<<<<< ${(ctx.method + ' ' + ctx.url).green} ${ctx.request.ip} <<<<<<<<<<<<<`)
+  // 给会话编个号
+  const id = ctx.state.id = count()
+
+  log(`[${id}] <<<<<<<<<<<<< ${(ctx.method + ' ' + ctx.url).green} ${ctx.request.ip} <<<<<<<<<<<<<`)
   debug(ctx.headers)
 
   await next()
 
-  log(`>>>>>>>>>>>>> ${(ctx.status + '')[ctx.status === 200 ? 'green' : 'red']} >>>>>>>>>>>>>`)
+  log(`[${id}] >>>>>>>>>>>>> ${(ctx.status + '')[ctx.status === 200 ? 'green' : 'red']} >>>>>>>>>>>>>`)
   debug(ctx.body)
 }
 
