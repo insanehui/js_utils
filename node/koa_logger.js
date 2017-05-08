2434a70b355035f16b6af515f0b777169f2701b2
insanehui <269200808@qq.com>
2017-05-09 00:08:51 +0800

    优化了一些日志格式

--- node/koa_logger.js
+++ node/koa_logger.js
@@ -2,12 +2,16 @@
  * koa一个打日志的小中间件
  */
 
-const debug = require('debug')('utils:koa_logger')
-const log_header = require('debug')('utils:koa_logger:header')
+const log = require('debug')('utils:koa_logger')
+const debug = require('debug')('debug:utils:koa_logger')
 
 export async function logger(ctx, next) { 
-  debug(`${ctx.method} ${ctx.url}`)
-  log_header(ctx.headers)
-  await next();
+  log(`<<<<<<<<<<<<< ${(ctx.method + ' ' + ctx.url).green} ${ctx.request.ip} <<<<<<<<<<<<<`)
+  debug(ctx.headers)
+
+  await next()
+
+  log(`>>>>>>>>>>>>> ${(ctx.status + '')[ctx.status === 200 ? 'green' : 'red']} >>>>>>>>>>>>>`)
+  debug(ctx.body)
 }
 
