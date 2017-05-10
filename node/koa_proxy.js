489413be75402e37409e0029a78705a774fcd866
insanehui <269200808@qq.com>
2017-05-10 17:40:21 +0800

    koa_proxy.js 与koa_logger对接

--- node/koa_proxy.js
+++ node/koa_proxy.js
@@ -9,60 +9,69 @@ const streamToPromise = require('stream-to-promise')
 
 import _ from 'lodash'
 
-const log = require('debug')('utils:koa_proxy');
-const debug = require('debug')('debug:utils:koa_proxy');
+const _log = require('debug')('utils:koa_proxy');
+const _debug = require('debug')('debug:utils:koa_proxy');
 
-// export const proxy = url => async ctx=>{ // 注意该中间件没有next，是一个封口件
+/*
+export const proxy = url => async ctx=>{ // 注意该中间件没有next，是一个封口件
 
-//   debug("req header", JSON.stringify(ctx.headers, null, '  '))
+  debug("req header", JSON.stringify(ctx.headers, null, '  '))
 
-//   const dest_url = Url.parse(url)
+  const dest_url = Url.parse(url)
 
-//   var opt = { 
+  var opt = { 
 
-//     // hostname: 'mart.cloudtogo.cn',
-//     // path: '/app/list',
-//     // port : 80,
+    // hostname: 'mart.cloudtogo.cn',
+    // path: '/app/list',
+    // port : 80,
 
-//     // hostname: 'localhost',
-//     // path: '/bar',
-//     // port : 8084,
+    // hostname: 'localhost',
+    // path: '/bar',
+    // port : 8084,
 
-//     // hostname: 'composer.dev.cloudtogo.cn',
-//     // path: '/get_blueprint',
-//     // port : 8084,
+    // hostname: 'composer.dev.cloudtogo.cn',
+    // path: '/get_blueprint',
+    // port : 8084,
 
-//     ...dest_url,
-//     path : dest_url.path + (ctx.querystring ? '?' + ctx.querystring : ''),
-//     method: ctx.method,
-//     headers : _.omit(ctx.headers, 'accept-encoding', 'host', ),
-//   }
+    ...dest_url,
+    path : dest_url.path + (ctx.querystring ? '?' + ctx.querystring : ''),
+    method: ctx.method,
+    headers : _.omit(ctx.headers, 'accept-encoding', 'host', ),
+  }
 
-//   log(`-> ${url}`)
-//   debug('req opt: ', opt)
+  log(`-> ${url}`)
+  debug('req opt: ', opt)
 
-//   const req = http.request(opt, (res) => {
-//     log(`<- ${url} res code: ${res.statusCode}`)
-//     debug(`res header: ${JSON.stringify(res.headers)}`)
+  const req = http.request(opt, (res) => {
+    log(`<- ${url} res code: ${res.statusCode}`)
+    debug(`res header: ${JSON.stringify(res.headers)}`)
 
-//     // 透传返回码以及headers
-//     ctx.status = res.statusCode
-//     ctx.set(res.headers)
-//   })
+    // 透传返回码以及headers
+    ctx.status = res.statusCode
+    ctx.set(res.headers)
+  })
 
-//   req.on('response', (response) => {
-//     response.pipe(ctx.res)
-//   })
+  req.on('response', (response) => {
+    response.pipe(ctx.res)
+  })
 
-//   ctx.req.pipe(req)
+  ctx.req.pipe(req)
 
-//   // req和res都要promise化才能正常工作
-//   await streamToPromise(ctx.req) // 这里传入req也是可以的
-//   await streamToPromise(ctx.res) 
+  // req和res都要promise化才能正常工作
+  await streamToPromise(ctx.req) // 这里传入req也是可以的
+  await streamToPromise(ctx.res) 
 
-// }
+}
+*/
 
 export const proxy2 = url => async (ctx, next)=>{ // 不封口的版本
+
+  const [log, debug] = (function(){
+    const x = ctx.state.logger
+    const log = x ? x(_log) : _log
+    const debug = x ? x(_debug) : _debug
+    return [log, debug]
+  })()
 
   debug("req header", JSON.stringify(ctx.headers, null, '  '))
 
