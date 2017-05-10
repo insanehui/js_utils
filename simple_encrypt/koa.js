5360c7a0f8328e7bce1eef369df21cc89f513d7c
insanehui <269200808@qq.com>
2017-05-10 18:48:19 +0800

    simple_encrypt/koa.js 使用 koa_logger

--- simple_encrypt/koa.js
+++ simple_encrypt/koa.js
@@ -9,7 +9,7 @@ export const req_xval = 'greedisgood'
 
 export const res_xkey = 'x-cloudtogo-web' // 返回的头，表示已加密。值可以为任意值，仅用来混淆视听:)
 
-const log = require('debug')('utils:simple_encrypt')
+const _log = require('debug')('utils:simple_encrypt')
 
 export async function simple_encrypt(ctx, next) {
 
@@ -27,6 +27,12 @@ export async function simple_encrypt(ctx, next) {
  * 于2017年5月2日重写simple_encrypt: 主要将加密的内容放在尾端
  */
 export async function koa(ctx, next){
+  const [log] = (function(){
+    const x = ctx.state.logger
+    const log = x ? x(_log) : _log
+    return [log]
+  })()
+
   await next() 
 
   if ( ctx.header[req_xkey] !== req_xval ) { // 仅当请求的http头未包含了暗号，则加密
