7522582f4a47bcc290cb4921b7c3a5c4ffba3772
insanehui <269200808@qq.com>
2017-05-31 19:01:06 +0800

    完善测试用例

--- components/utils.test.js
+++ components/utils.test.js
@@ -1,4 +1,17 @@
 import {parse_svg_transform} from './utils.js'
 
 describe('parse_svg_transform', () => {
+  
+  it('basic', () => {
+    const para = [
+    "tr() haha()"
+  ]
+    const hope = {
+    "tr": "tr()",
+    "haha": "haha()"
+  }
+    const fact = parse_svg_transform(...para)
+    expect(fact).toEqual(hope)
+  })
+
 });
