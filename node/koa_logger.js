21b0fdca31151804108e302ebaf86c153c0eb95a
insanehui <269200808@qq.com>
2017-06-13 16:39:59 +0800

    优化错误日志的打印

--- node/koa_logger.js
+++ node/koa_logger.js
@@ -8,7 +8,6 @@ const _log = require('debug')('utils:koa_logger')
 const _debug = require('debug')('debug:utils:koa_logger')
 
 import {count} from '../counter.js' // 计数器，用来统计会话
-import {tostr} from '../modash.js'
 
 // 会话id的背景色表，字体色固定为白色
 // 没有黄色是因为黄底白字看不清
@@ -36,7 +35,9 @@ export async function logger(ctx, next) {
 
   } catch(err) {
     ctx.status = 500
-    ctx.body = err
+    // 这里手动转为字符串的用意是，如果是一个错误类型，koa不会将其信息展示出来，为了保护技术细节
+    // 而目前出于调试方便的目的，暂时放开这些细节的暴露
+    ctx.body = err + '' 
   }
 
   const ok = ctx.status === 200
@@ -47,7 +48,7 @@ export async function logger(ctx, next) {
     debug(ctx.body)
   } else {
     // 前面预留两个空格主要是为了跟debug打的日志对齐
-    console.error(`  ${`${'ERROR'} ${tostr(ctx.body)}`.white.bgRed} ${id}`) 
+    console.error(`  ${`${'ERROR'} ${ctx.body}`.white.bgRed} ${id}`) 
   }
 }
 
