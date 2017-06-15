a653bcf0f84ea2a0949d1cc989b9b84e56233695
insanehui <269200808@qq.com>
2017-06-15 14:52:00 +0800

    删除旧代码，proxy2 转正为 proxy

--- node/koa_proxy.js
+++ node/koa_proxy.js
@@ -12,59 +12,7 @@ import _ from 'lodash'
 const _log = require('debug')('utils:koa_proxy');
 const _debug = require('debug')('debug:utils:koa_proxy');
 
-/*
-export const proxy = url => async ctx=>{ // 注意该中间件没有next，是一个封口件
-
-  debug("req header", JSON.stringify(ctx.headers, null, '  '))
-
-  const dest_url = Url.parse(url)
-
-  var opt = { 
-
-    // hostname: 'mart.cloudtogo.cn',
-    // path: '/app/list',
-    // port : 80,
-
-    // hostname: 'localhost',
-    // path: '/bar',
-    // port : 8084,
-
-    // hostname: 'composer.dev.cloudtogo.cn',
-    // path: '/get_blueprint',
-    // port : 8084,
-
-    ...dest_url,
-    path : dest_url.path + (ctx.querystring ? '?' + ctx.querystring : ''),
-    method: ctx.method,
-    headers : _.omit(ctx.headers, 'accept-encoding', 'host', ),
-  }
-
-  log(`-> ${url}`)
-  debug('req opt: ', opt)
-
-  const req = http.request(opt, (res) => {
-    log(`<- ${url} res code: ${res.statusCode}`)
-    debug(`res header: ${JSON.stringify(res.headers)}`)
-
-    // 透传返回码以及headers
-    ctx.status = res.statusCode
-    ctx.set(res.headers)
-  })
-
-  req.on('response', (response) => {
-    response.pipe(ctx.res)
-  })
-
-  ctx.req.pipe(req)
-
-  // req和res都要promise化才能正常工作
-  await streamToPromise(ctx.req) // 这里传入req也是可以的
-  await streamToPromise(ctx.res) 
-
-}
-*/
-
-export const proxy2 = url => async (ctx, next)=>{ // 不封口的版本
+export const proxy = url => async (ctx, next)=>{ // 不封口的版本
 
   const [log, debug] = (function(){
     const x = ctx.state.logger
