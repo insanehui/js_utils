be16351d836372cbdbd38ada960dfb2320fc8bcd
insanehui <269200808@qq.com>
2017-06-23 09:07:32 +0800

    add css func

---
+++ cssfunc.js
@@ -0,0 +1,16 @@
+/*
+ * css 装饰器函数
+ * cssobj的升级版，进一步贯彻函数式编程的思想
+ * 作为react组件的装饰器
+ */
+
+import {styler} from './components/utils.js'
+
+export const ellipsis = styler({
+  display: 'block',
+  whiteSpace: 'nowrap',
+  overflow : 'hidden',
+  textOverflow: 'ellipsis',
+})
+
+
