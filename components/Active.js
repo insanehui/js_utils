5d7c78d8a69cda603ad119fd92dd36308d9b4b57
insanehui <269200808@qq.com>
2017-07-24 16:51:08 +0800

    总结Active使用中的坑

--- components/Active.js
+++ components/Active.js
@@ -1,6 +1,11 @@
 /*
  * ActiveStyle的一个改进版的设计
  * 这个设计不会接受类似于 &:hover 这样的style属性，而是响应hover等事件，然后给其子组件（唯一）发送相应消息
+ * 使用注意事项：
+ * > 子组件（Cmp）必须要能够接收下面event对象里的所有事件（onFocus，onMouseEnter等）
+ * > 在子组件内部，其接收事件的成员，不能再接收这些事件，否则active将无法工作
+ * >> 解决方法一：将事件放到外面；方法二、子组件里分配另一个成员（另套一个div或者g）来接收相应的事件
+ *
  */
 
 import React, { PureComponent } from 'react'
