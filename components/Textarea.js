4aaace650b430338bac757315c4e7c3d90f54320
insanehui <269200808@qq.com>
2017-08-01 17:49:15 +0800

    解决换行以及空字符串的问题

--- components/Textarea.js
+++ components/Textarea.js
@@ -3,6 +3,7 @@
  */
 
 import React, { PureComponent } from 'react'
+import _ from 'lodash'
 import css from 'dom-css'
 
 import {merge_props as P} from './utils.js'
@@ -12,16 +13,21 @@ class Textarea extends PureComponent {
   state = {} // state用来存储动态的style
 
   update_shadow = np =>{ // 根据主输入框value的变化，更新shadow
-    const {value} = np
+    let {value} = np
     const {style} = this.props
-    const {shadow} = this
+    const {shadow, textarea} = this
+
+    // shadow会沿用textarea一些关键的样式配置
+    const text_style = _.pick(window.getComputedStyle(textarea),
+      'fontSize', 'fontFamily', 'whiteSpace', 'wordWrap'
+    )
+
     css(shadow, {
       ...style,
+      ...text_style,
 
       position: 'absolute',
       // top: -9999,
-
-      // 以下样式确保shadow和文本框的排版尺寸一致
 
       // 以下是临时测试
       top : 300,
@@ -29,10 +35,14 @@ class Textarea extends PureComponent {
       border : `1px solid gray`,
     })
 
+    if ( value.endsWith('\n') || !value ) { // 如果末尾有空行，或者为空
+      value += ' ' // 多加一个空格
+    } 
+
     shadow.innerHTML = value
-    const cstyle = window.getComputedStyle(shadow)
-    console.log("computed", cstyle)
-    const {height} = cstyle
+    const shadow_style = window.getComputedStyle(shadow)
+    console.log("computed", shadow_style)
+    const {height} = shadow_style
     this.setState({ height })
   }
 
@@ -52,9 +62,11 @@ class Textarea extends PureComponent {
 
   componentWillReceiveProps = this.update_shadow
 
+  ref = el=>{ this.textarea = el }
+
   render() {
     const props = P({style:this.state}, this.props)
-    return <textarea {...props}/>
+    return <textarea {...props} ref={this.ref}/>
   }
 }
 
