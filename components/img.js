7f413dfe92700e6013774d8c94cbcf267da661bc
insanehui <269200808@qq.com>
2017-08-15 17:21:33 +0800

    简单包装图片组件，阻止浏览器的缺省行为

---
+++ components/img.js
@@ -0,0 +1,38 @@
+/*
+ * 对img的一个简单的包装，简单地消除一些兼容性问题
+ */
+import React, { PureComponent } from 'react'
+
+class Base extends PureComponent {
+  onDragStart = e=>{
+    e.preventDefault()
+    const {onDragStart} = this.props
+    onDragStart(e)
+  }
+}
+
+
+export class Img extends Base {
+  render() {
+    const {alt = '', onDragStart, ...forward} = this.props
+    const p = {
+      onDragStart : this.onDragStart,
+      ...forward,
+    }
+    return <img alt={alt} {...p} />
+  }
+}
+
+export class Image extends Base { // svg里的image
+  render() {
+    const {onDragStart, ...forward} = this.props
+    const p = {
+      onDragStart : this.onDragStart,
+      ...forward,
+    }
+    return <image {...p} />
+  }
+}
+
+
+
