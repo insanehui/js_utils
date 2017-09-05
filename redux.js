31d339a1aa1eb7182f10b2d1677b30c31ccac699
insanehui <269200808@qq.com>
2017-09-05 10:52:47 +0800

    增加redux工具

---
+++ redux.js
@@ -0,0 +1,12 @@
+/*
+ * redux的一些工具
+ */
+
+import { connect as _connect } from 'react-redux'
+
+/*
+ * 一个包装版的connect，传入一个对象取代原来的参数的目的是可以使用装饰器
+ */
+export const connect = ({s, d}) => Cmp => _connect(s, d)(Cmp)
+
+
