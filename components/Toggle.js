b6a790dee84ed1b6790e01005f3760526e977e75
insanehui <269200808@qq.com>
2017-12-13 15:16:51 +0800

    使用上了portal，但是还是不好用啊

--- components/Toggle.js
+++ components/Toggle.js
@@ -3,14 +3,16 @@
  */
 
 import React, { PureComponent } from 'react'
+import {createPortal} from 'react-dom'
 
-function Cover(p) {
+function Cover({z, ...p}) {
   return <div {...p} style={{
     position: 'fixed',
     top: '0px',
     right: '0px',
     bottom: '0px',
     left: '0px',
+    zIndex : z,
   }} />
 }
 
@@ -24,8 +26,22 @@ export default class Toggle extends PureComponent {
     on : false,
   }
 
+  constructor(p) {
+    super(p)
+    // portal的dom元素要在构造器中准备好，如果放到componentDidMount中的话，已经晚了
+    this.cover = document.createElement('div') 
+
+    // 这行代码是该放在constructor还是componentDidMount里？各有什么区别？
+    document.body.prepend(this.cover)
+  }
+
+  componentWillUnmount(){
+    document.body.removeChild(this.cover)
+  }
+
   render() {
-    const {children, tag:Tag = 'div', ...forward} = this.props
+    const z0 = 99
+    const {children, tag:Tag = 'div', zIndexCover = z0, zIndexPopup = z0+1,  ...forward} = this.props
     const {on} = this.state 
 
     let [A,B] = React.Children.toArray(children) 
@@ -40,11 +56,15 @@ export default class Toggle extends PureComponent {
 
     B = React.cloneElement(B, {
       onClick : e=>e.stopPropagation(),
+      style : {
+        zIndex : zIndexPopup,
+        ...B.props.style,
+      }
     })
 
     return <Tag {...forward}>
       {A}
-      {on && <Cover onClick={e=>this.setState({ on : false })}/>}
+      {on && createPortal(<Cover z={zIndexCover} onClick={e=>this.setState({ on : false })}/>, this.cover)}
       {on && B}
     </Tag>
   }
