0a8a6a723df25113f9b01d4919a2a4d5866b1521
insanehui <269200808@qq.com>
2018-01-10 17:25:52 +0800

    初步增加BImg

---
+++ components/BImg.js
@@ -0,0 +1,40 @@
+/*
+ * 用backgroundImage机制来实现的img，可以解决常规img的一些不足，比如
+ * > 没有浏览器的拖动缺省行为
+ * > 可以不失真比例覆盖
+ */
+import React, { PureComponent } from 'react'
+import injectSheet from 'react-jss'
+
+@injectSheet({
+  cmain : {
+    backgroundRepeat : 'no-repeat',
+  }
+})
+export default class FlexImg extends PureComponent {
+  render() {
+    const {src, style, className, 
+      /*
+       * 当noflex为true时，会保持原图的比例裁剪并覆盖
+       */
+      noflex,
+      classes : {cmain},
+      ...rest} = this.props
+
+    const props = {
+      className : `${cmain} ${className}`,
+      style : {
+        backgroundImage : `url(${src})`,
+        ...(noflex ? {
+          backgroundSize : 'cover',
+          backgroundPosition : 'center center',
+        }: {
+          backgroundSize: '100% 100%',
+        }),
+        ...style,
+      },
+      ...rest,
+    }
+    return <div {...props}/>
+  }
+}
---
+++ components/BImg_test.js
@@ -0,0 +1,35 @@
+// React 
+import React, { PureComponent } from 'react'
+import {render,} from 'react-dom'
+import injectSheet from 'react-jss'
+import Img from './utils/components/BImg.js'
+
+@injectSheet({
+  cimg : {
+    transition : '0.4s',
+    width : '100%',
+    height : '100%',
+    '&:hover' : {
+      transform : `scale(1.5)`,
+    },
+  },
+  cdiv : {
+    width : 105,
+    height : 60,
+    overflow : 'hidden',
+    margin : 10,
+  },
+})
+class Test extends PureComponent {
+  render() {
+    const {classes:{cimg, cdiv}} = this.props
+    return <div>
+      <div className={cdiv}> <Img className={cimg} src='http://localhost:8085/news/b1.png' /> </div>
+      <div className={cdiv}> <Img className={cimg} noflex src='http://localhost:8085/news/b1.png' /> </div>
+    </div>
+  }
+}
+
+render(<Test />
+  , document.getElementById('root'))
+
