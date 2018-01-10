957444c95ca76857c86f7d10ba0453c1b847fe14
insanehui <269200808@qq.com>
2018-01-10 10:27:27 +0800

    完善一下Ellipsis的test代码

--- components/Ellipsis_test.js
+++ components/Ellipsis_test.js
@@ -9,19 +9,29 @@ import Ellipsis from './utils/components/Ellipsis.js'
   cmain : {
     backgroundColor : 'cyan',
     width : 100,
-    height : 100,
   },
 })
 class Test extends PureComponent {
+  state = {
+    height : 100,
+  }
+
   componentDidMount(){
   }
 
   render() {
     const {classes:{cmain}} = this.props
-    return <Ellipsis className={cmain}>
+    const {height} = this.state 
+
+    return <div>
+      <input type='number' value={height} onChange={e=>this.setState({ height:+e.target.value })}/>
+      <Ellipsis className={cmain} style={{
+        height,
+      }} >
 本课程介绍了数字化业务创新的背景下，云服务开发技术方面的知识，如前端开发语言基础、后端开发语言基础、敏捷开发基础、开
 本课程介绍了VoLTE业务相关的IMS技术与信令流程，EPC网络原理，SIP协议，VoLTE部署与关键特性等，让学员
-    </Ellipsis>
+      </Ellipsis>
+    </div>
   }
 }
 
