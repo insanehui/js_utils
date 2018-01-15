ddfcef8ccb1defcae62e107ee8afd9032f03c362
insanehui <269200808@qq.com>
2018-01-15 14:38:29 +0800

    优化了一下错误信息的输出

--- node/koa_logger.js
+++ node/koa_logger.js
@@ -38,19 +38,15 @@ export async function logger(ctx, next) {
     // 这里手动转为字符串的用意是，如果是一个错误类型，koa不会将其信息展示出来，为了保护技术细节
     // 而目前出于调试方便的目的，暂时放开这些细节的暴露
     ctx.body = err + '' 
+
+      // 前面预留两个空格主要是为了跟debug打的日志对齐
+    console.error(`  ${`${'ERROR'} ${ctx.body}`.white.bgRed} ${id}`) 
+    return
   }
 
   if ( ctx.respond ) {
-    const ok = ctx.status === 200
-
     log(`>>>>>>>>>>>>> ${(ctx.status + '')[ok ? 'green' : 'red']} >>>>>>>>>>>>>`)
-
-    if ( ok ) {
-      debug(ctx.body)
-    } else {
-      // 前面预留两个空格主要是为了跟debug打的日志对齐
-      console.error(`  ${`${'ERROR'} ${ctx.body}`.white.bgRed} ${id}`) 
-    }
+    debug(ctx.body)
   } 
   else {
     log('>>>> request handled outside koa >>>>>>'.cyan)
