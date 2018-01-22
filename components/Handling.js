8b439a18d5bbe0364db1f731277ccbbbda69ce33
insanehui <269200808@qq.com>
2018-01-22 19:13:49 +0800

    增加disable控制，不能重复点击

--- components/Handling.js
+++ components/Handling.js
@@ -5,6 +5,11 @@ import React, { PureComponent } from 'react'
 import {func} from 'prop-types'
 import _ from 'lodash'
 
+const disable = e=>{
+  e.preventDefault()
+  e.stopPropagation()
+}
+
 export default class Handling extends PureComponent {
   state = { }
 
@@ -14,9 +19,13 @@ export default class Handling extends PureComponent {
 
   render() {
     let {children:fn, as:As, ...props} = this.props
+    const {state} = this
 
     props = _.mapValues(props, (v, name)=>{
-      if (/^on[A-z]/.test(name)) {
+      if ( state[name] ) { // 如果已经有状态，说明事件正在处理中，直接返回disable
+        return disable
+      } 
+      if (/^on[A-z]/.test(name)) { // 否则如果是事件属性，hook一下
         const handler = v
         return async e=>{
           this.setState({ [name]:true })
@@ -24,11 +33,12 @@ export default class Handling extends PureComponent {
           this.setState({ [name]:false })
         }
       } 
+      // 缺省情况原样返回
       return v
     })
 
     return <As {...props}>
-      {fn(this.state)}
+      {fn(state)}
     </As>
   }
 }
