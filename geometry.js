696f8321b13de2b44310e3a659116fa51ee459bb
insanehui <269200808@qq.com>
2018-01-30 10:59:27 +0800

    增加is_rect_intersect

--- geometry.js
+++ geometry.js
@@ -225,3 +225,10 @@ export function pre_traverse(tree, fn){
   traverse(tree)
 }
 
+// 判断两个rect是否相交
+export function is_rect_intersect(r1, r2){
+  return !(r2.left > r1.right || 
+           r2.right < r1.left || 
+           r2.top > r1.bottom ||
+           r2.bottom < r1.top)
+}
