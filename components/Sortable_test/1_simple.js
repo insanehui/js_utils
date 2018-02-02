8c6e2adb7f62b52cd713712b917609726d31aeae
insanehui <269200808@qq.com>
2018-02-02 10:59:33 +0800

    增加注释

--- components/Sortable_test/1_simple.js
+++ components/Sortable_test/1_simple.js
@@ -18,9 +18,15 @@ class Test extends PureComponent {
       height : 600,
     }}>
       <Sortable value={value} onChange={v=>this.setState({ value:v })}>
-        {item=>(<div>
-          {item.value}
-        </div>)}
+        {({value:itemValue, onChange, sortIndex})=>{ 
+          /*
+           * 注入三个属性，onChange的作用是用于将局部的修改汇报给外层
+           * 本例中没有用到，参见with_formy的例子
+           */
+          return <div>
+            {itemValue}
+          </div> 
+        }}
       </Sortable>
     </div>
   }
