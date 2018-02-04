a0a1c60bd824e01fcc6e3e3fa432bc8b42a6b9fb
insanehui <269200808@qq.com>
2018-02-04 16:58:31 +0800

    注释

--- components/uncontrolled_test.js
+++ components/uncontrolled_test.js
@@ -22,7 +22,8 @@ class Test extends PureComponent {
     /*
      * free的神奇功效：原本是controlled控件的Sortable，被free了之后，就可以直接丢给它一个value
      * 它就可以生活自理了（自己管理状态）
-     * 外界通过实例暴露的value
+     * 外界通过实例暴露的value来读取、修改其状态
+     * onChange事件仍然可用
      */
     return <div>
       <button onClick={e=>console.log('value', this.refs.sortable.value)}>查看</button>
