ab96854185c34ba23f9f5505c3ba48246a95c7b3
insanehui <269200808@qq.com>
2018-02-05 20:52:36 +0800

    增加localStoragify和valuefy

---
+++ components/valuefy.js
@@ -0,0 +1,29 @@
+/*
+ * 将e.target.value的原生控件改为标准的value的形式
+ * 要求为controlled形式
+ */
+import React, { PureComponent } from 'react'
+import {displayName} from './displayName.js'
+
+export const valuefy = Control => {
+  class Valuefied extends PureComponent{
+
+    static displayName = `${process.env.NODE_ENV === 'development' ? 'valuefy' : 'vfy'}(${displayName(Control)})`
+
+    static defaultProps = {
+      onChange : ()=>{},
+    }
+
+    render(){
+      let {onChange, value, ...rest} = this.props
+
+      if ( value === undefined && Control === 'input' ) {
+        value = ''
+      } 
+
+      return <Control {...{...rest, value}} onChange={e=>onChange(e.target.value)} />
+    }
+  }
+
+  return Valuefied
+}
