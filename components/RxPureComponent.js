d1c011d2445002fce519512223e8b9be68a93e1d
insanehui <269200808@qq.com>
2018-02-23 16:19:10 +0800

    增加RxPureComponent基类

---
+++ components/RxPureComponent.js
@@ -0,0 +1,35 @@
+/*
+ * 可提供一些rx便利的react组件基类
+ */
+import { PureComponent } from 'react'
+import {findDOMNode} from 'react-dom'
+
+import Rx from 'rxjs/Rx'
+
+export default class RxPureComponent extends PureComponent {
+  constructor(p) {
+    super(p)
+    const events = [
+      'mousedown', 'click',
+    ]
+    for (const event of events) {
+      this[`${event}`] = node => this.event(node, event)
+    }
+  }
+
+  subscription = []
+
+  event = (node, event) => {
+    return Rx.Observable.fromEvent(findDOMNode(node), event) // so we can pass in a react inst
+  }
+
+  subscribe = (stream, func) => {
+    this.subscription.push( stream.subscribe(func) )
+  }
+
+  componentWillUnmount(){
+    for (const sub of this.subscription) {
+      sub.unsubscribe()
+    }
+  }
+}
---
+++ components/RxPureComponent_test.js
@@ -0,0 +1,39 @@
+// React
+/*
+ * 使用上自己封装的RxPureComponent
+ */
+import React, {PureComponent} from 'react'
+import { render } from 'react-dom'
+import RxPureComponent from './utils/components/RxPureComponent.js'
+
+class Sub extends RxPureComponent {
+
+  componentDidMount(){
+    const {click, subscribe} = this
+    const {button} = this.refs
+    subscribe(click(button), () => console.log('Clicked!'))
+  }
+
+  render() {
+    return <div>
+      <button ref='button'>点击一下</button>
+    </div>
+  }
+}
+
+class App extends PureComponent {
+  state = {
+    on : false,
+  }
+
+  render() {
+    const {on} = this.state 
+    return <div>
+      <button onClick={e=>this.setState({ on : !on })}>Toggle</button>
+      {on && <Sub />}
+    </div>
+  }
+}
+
+render(<App />, document.getElementById('root'))
+
