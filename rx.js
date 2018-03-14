bb43a395440b226fe42a8aac21b3c36e9a75cfc7
insanehui <269200808@qq.com>
2018-03-14 18:50:23 +0800

    更新一下drag的实现

--- rx.js
+++ rx.js
@@ -2,7 +2,7 @@ import {findDOMNode} from 'react-dom'
 import Rx from 'rxjs/Rx'
 
 import './rx/mouseDxy.js'
-import './rx/clip.js'
+// import './rx/clip.js'
 
 /*
  * 拖动流，可防"脱手"
@@ -20,7 +20,8 @@ export const drag = el=>{
   const ups = Rx.Observable.fromEvent(window, 'mouseup')
 
   const downs = Rx.Observable.fromEvent(el, 'mousedown')
-  return moves.clip(downs, ups)
+  // return moves.clip(downs, ups)
+  return downs.flatMap(e=>moves.takeUntil(ups)) // 这是从rx官方示例里找到的代码，代替clip
 }
 
 
