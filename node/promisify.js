810444de282f3839bf4e78722f8664bfbe4b2f0e
insanehui <269200808@qq.com>
2018-03-28 14:39:49 +0800

    改进了promisified exec的实现

--- node/promisify.js
+++ node/promisify.js
@@ -2,7 +2,17 @@
  * 增加一些经过promisify的原生库函数
  * TODO: 后续如果这里的函数多了，再按包名进行区分
  */
-const util = require('util');
-const cp = require('child_process')
+// const util = require('util');
+const child_process = require('child_process')
 
-export const exec = util.promisify(cp.exec);
+// export const exec = util.promisify(cp.exec); // 原来直接调用官方提供的promisify
+
+// 支持传入标准输入的exec
+export const exec = (cmd, input)=>new Promise((ok, err)=>{
+  const sub = child_process.exec(cmd, (error, stdout, stderr)=>{
+    ok({error, stdout, stderr})
+  })
+  if ( input !== undefined ) {
+    sub.stdin.end(input)
+  } 
+})
