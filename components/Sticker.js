48bbbb7ad482d74b94a00fa325d5eb4883278b2a
insanehui <269200808@qq.com>
2018-04-28 12:12:16 +0800

    初步提取一个calcOffset函数

--- components/Sticker.js
+++ components/Sticker.js
@@ -2,6 +2,31 @@ import React, { PureComponent } from 'react'
 import {findDOMNode, createPortal} from 'react-dom'
 import _ from 'lodash'
 
+
+/*
+ * 通过自身的位置和参数物的位置计算出需要偏移的量: my - ref = {dx, dy}
+ * my和ref是dom元素
+ */
+function calcOffset(my, ref) {
+  /*
+   * 暂时只考虑右边和下方是否越界
+   */
+  const myPos = _.pick(my.getBoundingClientRect(), 'left', 'bottom', 'top', 'right')
+  const prevPos = _.pick(ref.getBoundingClientRect(), 'left', 'bottom', 'top', 'right')
+
+  /*
+   * 算出要往"上"挪的距离
+   */
+  const dy = myPos.top - prevPos.bottom
+
+  /*
+   * 算出要往"左"挪的距离
+   */
+  const dx = myPos.left - prevPos.left
+
+  return {dx, dy}
+}
+
 /*
  * 根据参考物的位置来决定自己的位置，并且通过定时的机制来自动跟随
  * 暂时固定为将同级的兄长作为参照物
@@ -24,6 +49,12 @@ export default class Sticker extends PureComponent {
     bottom : 0,
   }
 
+  static defaultProps = {
+    by : 1,
+    dx : 0,
+    dy : 0,
+  }
+
   componentDidMount(){
     this.sensor()
   }
@@ -33,9 +64,18 @@ export default class Sticker extends PureComponent {
     this.el = null
   }
 
-  adjust = ()=>{
+  setOffset = offset =>{
     let {right, bottom} = this.state 
-    const {by = 1, dy = 0} = this.props
+    const {dx, dy} = this.props
+    right += (offset.dx + dx)
+    right = Math.max(0, right)
+    bottom += (offset.dy + dy)
+    bottom = Math.max(0, bottom)
+    this.setState({ right, bottom })
+  }
+
+  adjust = ()=>{
+    const {by} = this.props
 
     const me = findDOMNode(this)
     let prev = me
@@ -46,23 +86,9 @@ export default class Sticker extends PureComponent {
     /*
      * 暂时只考虑右边和下方是否越界
      */
+    const offset = calcOffset(this.el.children[0], prev)
 
-    const myPos = _.pick(this.el.children[0].getBoundingClientRect(), 'left', 'bottom', 'top', 'right')
-    const prevPos = _.pick(prev.getBoundingClientRect(), 'left', 'bottom', 'top', 'right')
-
-    /*
-     * 算出要往"上"挪的距离，不能为负
-     */
-    bottom += myPos.top - prevPos.bottom - dy
-    bottom = Math.max(0, bottom)
-
-    /*
-     * 算出要往"左"挪的距离，同样也不能为负
-     */
-    right += myPos.left - prevPos.left
-    right = Math.max(0, right)
-
-    this.setState({ bottom, right })
+    this.setOffset(offset)
   }
 
   sensor = ()=>{ // 部署一个sensor监听画布的尺寸信息，以计算出工具栏应放置的位置
@@ -81,10 +107,18 @@ export default class Sticker extends PureComponent {
       /*
        * 默认情况下Sticker会紧贴着其参照物，可以通过设置dy来控制偏移间隔
        */
-      dy,
+      /*
+       * sticker所在的方位，缺省在下方
+       */
+      side = 'bottom',
       ...forward} = this.props
     const {right, bottom} = this.state 
 
+    /*
+     * 注：这种通过forward的方式传属性的方式尽量还是不要采用了
+     * 而且style的合并也晦涩，增加逻辑负担
+     * TODO: 后续有时间将其改掉
+     */
     const props = {
       ...forward,
       style : {
