5dee16a2cbc54fa8b7d7f630a685cda26815e91c
insanehui <269200808@qq.com>
2018-05-11 14:11:00 +0800

    更名为refIt

--- components/utils/refIt.js
+++ components/utils/refIt.js
@@ -1,12 +1,12 @@
 /*
- * 加上该装饰器之后，就可以使用this.ref('xxx')来将ref绑定到 this.xxx
+ * 加上该装饰器之后，就可以使用ref={this.refMe('xxx')}来将ref绑定到 this.xxx
  */
 
 export default base => {
   return class RefHelper extends base {
     constructor(p) {
       super(p)
-      this.ref = (name = 'refed') => el=>{
+      this.refMe = (name = 'refed') => el=>{
         this[name] = el
       }
     }
