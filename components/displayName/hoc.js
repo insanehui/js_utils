1b4527a2c65ca894bfb5cc08894dcf0df4b97bc8
insanehui <269200808@qq.com>
2018-05-14 17:48:51 +0800

    增加一个为hoc取名的工具

---
+++ components/displayName/hoc.js
@@ -0,0 +1,10 @@
+/*
+ * displayName的包装，用于hoc的设计中
+ */
+import displayName from './get.js'
+import isDev from '../../web/is_dev.js'
+
+export default (Cmp, name, name_prod = name) => {
+  name = isDev ? name : name_prod
+  return `${displayName(Cmp)}@${name}`
+}
