5e32c6bd7cc3888dd3a533175e5dfcc52a6c1cef
insanehui <269200808@qq.com>
2018-05-15 16:42:47 +0800

    editable增加blur的一些选项

--- components/Editable2.js
+++ components/Editable2.js
@@ -15,25 +15,39 @@ class Editable extends PureComponent {
   static defaultProps = {
     tag : 'span',
     autoSelect : true, // 激活输入模式是否自动选择
+    onChange : ()=>{},
+    trigger : 'onClick',
+    blurConfirm : false, // blur时是否确认修改
   }
 
   on = eclose(()=>this.setState({ is_editing : true }))
 
   off = ()=>this.setState({ is_editing : false })
 
-  editKeyDown = e=>{
-    const {onChange, value} = this.props
-    const {keyCode} = e
-    const {ENTER} = KeyCode
+  confirm = ()=>{
+    this.off()
     const {input} = this
+    const {value, onChange} = this.props
+    if ( value !== input.value ) {
+      onChange(input.value)
+    } 
+  }
+
+  editKeyDown = e=>{
+    e.stopPropagation()
+    const {keyCode} = e
+    const {ENTER, ESC} = KeyCode
 
     if (keyCode === ENTER ) {
-      this.off()
-      if ( value !== input.value ) {
-        onChange && onChange(input.value)
-      } 
+      this.confirm()
     }
-    // esc键会自动触发blur消息
+    /*
+     * esc键会自动触发blur消息.  
+     * 坑：如果chrome启用了vimium插件，ESC键的功能将不起作用
+     */
+    else if ( keyCode === ESC ) { 
+      this.off()
+    } 
   }
 
   input_ref = el=>{ 
@@ -47,7 +61,7 @@ class Editable extends PureComponent {
   } 
 
   Input = ()=>{
-    const {value} = this.props
+    const {value, blurConfirm} = this.props
     const input = {
       defaultValue : value,
       style : {
@@ -58,18 +72,17 @@ class Editable extends PureComponent {
       },
       ref : this.input_ref,
       onKeyDown : this.editKeyDown,
-      onBlur : this.off,
+      onBlur : blurConfirm ? this.confirm : this.off,
     }
     return <input {...input}/>
   }
 
   render() {
-    const {tag, value,  
-      trigger = 'onClick',
-      autoSelect, onChange, // filter
+    const {tag:Tag, value,  
+      trigger,
+      autoSelect, onChange, blurConfirm, // filter
       ...forward} = this.props
     const {is_editing} = this.state 
-    const Tag = tag
 
     const props = {
       ...forward,
