d2cafcbed77e1afd6aa714a17b6e2ea999c97dcb
insanehui <269200808@qq.com>
2018-05-16 17:03:55 +0800

    支持多重弹框

--- components/Prompt.js
+++ components/Prompt.js
@@ -2,60 +2,68 @@
  * 用react实现的window.prompt async版本
  */
 import React, { PureComponent } from 'react'
-import {render,} from 'react-dom'
+import {render,unmountComponentAtNode} from 'react-dom'
 import Modal from 'react-modal'
 import {promisify} from '../modash.js'
 
-// 先占位一个放置modal的dom元素
-const dialog_div = document.createElement('div')
-document.body.appendChild(dialog_div)
-
-/*
- * 接收一个controlled的编辑器（value, onChange(支持v或者是event)）
- */
-class Prompt extends PureComponent {
-
-  constructor(p) {
-    super(p)
-    const {isOpen} = p
-    this.state = {isOpen}
-  }
-
-  componentWillReceiveProps(np) {
-    const {isOpen} = np
-    this.setState({isOpen})
-  }
-
-  close = ()=>{
-    this.setState({ isOpen : false })
-  }
-
-  render() {
-    const {isOpen} = this.state
-    const {onDone, value, Edit, modalProps} = this.props
-    const {cancel} = this
-    const modal = {
-      ...modalProps,
-      isOpen,
-      appElement : document.body,
-      onRequestClose : cancel,
-    }
-
-    const edit = {
-      value,
-      onChange : v=>{
-        onDone(v)
-        this.close()
-      }
-    }
-
-    return <Modal {...modal}>
-      <Edit {...edit}/>
-    </Modal>
-  }
-}
-
 function prompt_cb(resolve, reject, value, Edit, modalProps) {
+  /*
+   * Edit为一个controlled的编辑器（value, onChange(支持v或者是event)）
+   * 对话框关闭则销毁一切资源，"用完即走"，因此把类写在闭包里
+   * 支持多重弹框
+   */
+
+  const dialog_div = document.createElement('div')
+  document.body.appendChild(dialog_div)
+
+  class Prompt extends PureComponent {
+
+    constructor(p) {
+      super(p)
+      const {isOpen} = p
+      this.state = {isOpen}
+    }
+
+    componentWillReceiveProps(np) {
+      const {isOpen} = np
+      this.setState({isOpen})
+    }
+
+    close = ()=>{
+      this.setState({ isOpen : false }, ()=>{
+        /*
+         * 手动unmount，就像小程序一样，用完即走
+         */
+        unmountComponentAtNode(dialog_div)
+        document.body.removeChild(dialog_div)
+      })
+    }
+
+    render() {
+      const {isOpen} = this.state
+      const {onDone, value, Edit, modalProps} = this.props
+      const {cancel} = this
+      const modal = {
+        ...modalProps,
+        isOpen,
+        appElement : document.body,
+        onRequestClose : cancel,
+      }
+
+      const edit = {
+        value,
+        onChange : v=>{
+          onDone(v)
+          this.close()
+        }
+      }
+
+      return <Modal {...modal}>
+        <Edit {...edit}/>
+      </Modal>
+    }
+  }
+
   const props = {
     isOpen : true,
     value,
