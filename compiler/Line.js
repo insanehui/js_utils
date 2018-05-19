4f3669b47b4582c6e75504e0c0eddad39da57732
insanehui <269200808@qq.com>
2018-05-19 13:38:22 +0800

    init compiler

---
+++ compiler/Line.js
@@ -0,0 +1,13 @@
+import Unit from './Unit.js'
+
+export default class Line extends Unit {
+  read = str=>{
+    /*
+     * 这里有个陷阱，正好歪打正着：如果str里没有包含换行，则会consume掉整个字符串
+     * 因为下面的正则没有匹配到，所以str原样返回
+     */
+    const match = str.replace(/(^.*\n)[\s\S]*/m, '$1')
+    this.data = match
+    return str.slice(match.length)
+  }
+}
---
+++ compiler/Unit.js
@@ -0,0 +1,9 @@
+/*
+ * 基础类
+ */
+export default class Unit {
+  data = '' // 字符
+  toString = ()=>{
+    return this.data
+  }
+}
