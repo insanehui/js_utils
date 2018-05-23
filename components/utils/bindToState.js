d135090cb9f4b907a07e42bf1585f7b14619ae6b
insanehui <269200808@qq.com>
2018-05-23 20:49:09 +0800

    初步写了bindToState，未测试

---
+++ components/utils/bindToState.js
@@ -0,0 +1,22 @@
+/*
+ * 将一个控件的值绑定到父组件的state里。
+ * 提供三种形式：
+ *  散函数
+ *  hoc装饰器
+ *  外壳组件
+ */
+import _ from 'lodash'
+
+// hoc装饰器
+export default (methodName='bindState') => base => {
+  class binder extends base {
+    constructor(p) {
+      super(p)
+      this[methodName] = stateProp => ({
+        value : _.get(this.state, stateProp),
+        onChange : v=>this.setState({ [stateProp] : v })
+      })
+    }
+  }
+  return binder
+}
