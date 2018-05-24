53bea40fdeeb0205c5eac278f95073eaa66575e4
insanehui <269200808@qq.com>
2018-05-24 16:52:23 +0800

    validate on blur巧妙地forward了ref

--- components/Formy/validateOnBlur.js
+++ components/Formy/validateOnBlur.js
@@ -2,29 +2,44 @@
  * 在blur的时候进行validate
  */
 
-import React, { PureComponent } from 'react'
+import React, { PureComponent, forwardRef } from 'react'
 
 /*
  * 要求Cmp为Validatable
+ * 
+ * 注：这里为了做到能forward ref，想了一个偏方:
+ * 先把ref传给this保管一个，然后马上把外面的forward进来
  */
 export default Cmp => {
+  return forwardRef((props, ref)=>{
+    class ValidateOnblur extends PureComponent {
+      static defaultProps = {
+        onBlur : x=>x,
+      }
 
-  class ValidateOnblur extends PureComponent {
-    static defaultProps = {
-      onBlur : x=>x,
+      state = {
+        forwardedRef : null,
+      }
+
+      onBlur = e=>{
+        const {onBlur} = this.props
+        this.inner.checkValidity()
+        onBlur(e)
+      }
+
+      localRef = el=>{
+        if ( el ) {
+          this.inner = el
+        } 
+        this.setState({ forwardedRef : ref })
+      }
+
+      render() {
+        const {onBlur, localRef} = this
+        const {forwardedRef} = this.state 
+        return <Cmp {...this.props} ref={forwardedRef || localRef } onBlur={onBlur}/>
+      }
     }
-
-    onBlur = e=>{
-      const {onBlur} = this.props
-      this.refs.inner.checkValidity()
-      onBlur(e)
-    }
-
-    render() {
-      const {onBlur} = this
-      return <Cmp {...this.props} ref='inner' onBlur={onBlur}/>
-    }
-  }
-
-  return ValidateOnblur
+    return <ValidateOnblur {...props} />
+  })
 }
