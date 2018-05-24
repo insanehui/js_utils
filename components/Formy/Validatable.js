5e9d9a232d415f634e0248b709559f4645a7caef
insanehui <269200808@qq.com>
2018-05-24 15:00:21 +0800

    尝试增加一些validation相关的东西

--- components/Formy/Validatable.js
+++ components/Formy/Validatable.js
@@ -4,12 +4,16 @@
  * 注：
  *  通过一个onValid()回调，似乎也是一种方式，目前暂时无法凭空判断哪种方法更佳，姑且先采用inst method的方法
  */
-import React, {forwardRef, PureComponent} from 'react'
-import {bindState} from '../utils/bindToState.js'
+import React, {
+  // forwardRef, 
+  PureComponent, 
+  // Fragment, 
+  createRef
+} from 'react'
 
 // hoc
-const validatify = El => {
-  class Validatable extends PureComponent {
+export const validatify = El => {
+  class Validity extends PureComponent {
     /*
      * 实例方法
      */
@@ -18,8 +22,55 @@ const validatify = El => {
     }
 
     render(){
-      <El {...this.props} ref='el' />
+      return <El {...this.props} ref='el' />
+    }
+  }
+  return Validity
+}
+
+/*
+ * 要求El为validatified
+ */
+export const validatable = render => El => {
+  class Validatable extends PureComponent {
+    constructor(p) {
+      super(p)
+      this.ref = createRef()
+    }
+
+    state = {
+      invalid : false,
+    }
+
+    checkValidity = (on = true)=>{
+      const {valid} = this.ref.current.validity()
+      this.setState({ invalid : !valid })
+    }
+
+    render() {
+      const {invalid} = this.state 
+      return render(El, this.props, this.ref, invalid)
     }
   }
   return Validatable
 }
+
+  /*
+class Validable extends PureComponent {
+  state = {
+    invalid : false,
+  }
+
+  checkValidity = (on = true)=>{
+    const {valid} = this.refs.el.validity()
+    this.setState({ invalid : !valid })
+  }
+
+  render() {
+    const {invalid} = this.state 
+    return <input {...this.props} ref='el' style={{
+      borderColor : invalid ? 'red' : 'gray',
+    }} />
+  }
+}
+*/
