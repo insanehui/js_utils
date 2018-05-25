3a463039e89b40f3790f2d1cb8305c366f5410f7
insanehui <269200808@qq.com>
2018-05-25 09:38:21 +0800

    初步将validation的组件重命名了

---
+++ components/Formy/validation/checker.js
@@ -0,0 +1,26 @@
+/*
+ * 此处制定一个统一的用于校验的接口规范。
+ * 定义一个Validity Checkable的概念：
+ * > 为Formy标准的控件. 原生的input等元素需要adaptor包装
+ * > 具有validity()方法. 原生的input等元素需要本模块提供的装饰器包装
+ */
+
+/*
+ * hoc, 赋予validity()方法 
+ */
+export default El => {
+  class ValidityCheckable extends PureComponent {
+    /*
+     * 实例方法
+     */
+    validity = ()=>{
+      return this.refs.el.validity
+    }
+
+    render(){
+      return <El {...this.props} ref='el' />
+    }
+  }
+  return ValidityCheckable
+}
+
---
+++ components/Formy/validation/reactor.js
@@ -0,0 +1,50 @@
+/*
+ * ValidityReactor: 
+ * > 具有checkValidity(boolean, ...扩展参数)
+ * > 该方法不返回值，只会引起组件的展示形态的变化，比如可用来展示一些校验的提示信息等
+ */
+
+import React, { PureComponent, createRef } from 'react'
+
+/*
+ * 要求El为ValidityCheckable, 在此基础上赋予checkValidity()方法
+ */
+export const validatable = render =>  
+  /*
+  * (El, props, ref, invalid)
+  * ref参数用来forward用
+  */ 
+  El => {
+  class Validatable extends PureComponent {
+    constructor(p) {
+      super(p)
+      this.ref = createRef()
+    }
+
+    state = {
+      invalid : false,
+    }
+
+    /*
+     * forward一下 validity()方法
+     */
+    validity = ()=>{
+      return this.ref.current.validity()
+    }
+
+    /*
+     * on = true代表开启校验，否则为关闭校验
+     */
+    checkValidity = (on = true)=>{
+      const {valid} = this.validity()
+      this.setState({ invalid : on && !valid })
+    }
+
+    render() {
+      const {invalid} = this.state 
+      return render(El, this.props, this.ref, invalid)
+    }
+  }
+  return Validatable
+}
+
