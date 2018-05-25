9b0196ec2534c44a7a08ff146f7676fe1b3d67dd
insanehui <269200808@qq.com>
2018-05-25 16:43:21 +0800

    想实现一个binder，但发现还依赖onChange的扩展。。。

--- components/Formy/validation/checker.js
+++ components/Formy/validation/checker.js
@@ -5,6 +5,7 @@
  * > 具有validity()方法. 原生的input等元素需要本模块提供的装饰器包装
  */
 import React, { PureComponent } from 'react'
+import _ from 'lodash'
 import {Input as input} from '../Adapted.js'
 
 /*
@@ -30,3 +31,25 @@ export default hoc
 
 export const Input = hoc(input)
 
+/*
+ * 跟组件state绑定的一个工具函数
+ */
+// export const bindState = ctx => opt => {
+//   const {validity} = opt
+//   let ret = {}
+//   if ( opt.value ) {
+//     // TODO
+//   } 
+//   if ( validity ) {
+//     const name = _.isString(validity) ? validity : 'validity',
+//     /*
+//      * TODO 允许用户传入一个onChange，并对其接管
+//      */
+//     ret = {
+//       onChange : ()=>{
+//         ctx.setState({[name] : })
+//       }
+//     }
+//   } 
+//   return ret
+// }
