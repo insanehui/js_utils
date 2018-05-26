edd0dc5c070849003d1124c683a6ca699709bfb1
insanehui <269200808@qq.com>
2018-05-26 18:13:36 +0800

    临时提交form1

---
+++ components/Formy/test/form1.js
@@ -0,0 +1,40 @@
+/*
+ * 复制antd表单示例里的第一个（最简单场景）
+ */
+import React, { PureComponent, Fragment as F} from 'react'
+import _ from 'lodash'
+import {compose} from 'ramda'
+
+import {render} from 'react-dom'
+import {free as Form} from './utils/components/Formy/Form.js'
+import {bindState as $} from './utils/components/Formy/validation/checker.js'
+import {change} from './utils/components/Formy/validation/validateOn.js'
+import {Input as reactor} from './utils/components/Formy/validation/reactor.js'
+
+const R = reactor((C, props, ref, invalid,)=>{
+  return <F>
+    <C {...props} ref={ref} />
+    {invalid && <span style={{color:'red'}} >请输入正确的值</span>}
+  </F>
+})
+
+const Input = compose(
+  change,
+)(R)
+
+class Test extends PureComponent {
+  render() {
+    const valid = _.get(this.state, 'valid')
+
+    return <Form ref='form' {...$(this)({valid:1})} >
+      <Input name='a' pattern='aaa' required placeholder='输入"aaa"' />
+      <Input name='b' pattern='bbb' required placeholder='输入"bbb"' />
+      <input name='c' pattern='ccc' required placeholder='输入"ccc",此框不参与校验' />
+      <button disabled={!valid} onClick={()=>window.alert(JSON.stringify(this.refs.form.value, null, '  '))}>提交</button>
+    </Form>
+  }
+}
+
+render(<Test /> , document.getElementById('root'))
+
+
