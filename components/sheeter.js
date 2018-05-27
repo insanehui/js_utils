877c5712600f8077603a3fd68b9d0de258dc715b
insanehui <269200808@qq.com>
2018-05-27 18:47:59 +0800

    sheeter可以forward ref

--- components/sheeter.js
+++ components/sheeter.js
@@ -1,8 +1,9 @@
 /*
  * 用jss的方式给一个组件注入样式
- * 相较于styler，可以解决一些hover等样式问题
+ * 相较于styler，可以解决一些hover等样式问题. 
+ * > forward ref
  */
-import React, { PureComponent } from 'react'
+import React, { PureComponent, forwardRef } from 'react'
 import injectSheet from 'react-jss'
 
 export default style => (Cmp='div') => {
@@ -12,11 +13,12 @@ export default style => (Cmp='div') => {
   })
   class Sheeter extends PureComponent {
     render() {
-      const {classes:{cmain}, className, ...rest} = this.props
+      const {classes:{cmain}, sheet, className, xRef, ...rest} = this.props
       let cls = className ? ' '+className : ''
-      return <Cmp className={`${cmain}${cls}`} {...rest} />
+      return <Cmp className={`${cmain}${cls}`} {...rest} ref={xRef} />
     }
   }
-  return Sheeter
+
+  return forwardRef((props, ref)=><Sheeter {...props} xRef={ref} />)
 }
 
