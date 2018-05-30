821cad5beaf90586a4f0874cc1decda572e0e9fb
insanehui <269200808@qq.com>
2018-05-30 19:14:04 +0800

    扩展insertable的测试

--- components/Insertable_test.js
+++ components/Insertable_test.js
@@ -4,26 +4,58 @@ import injectSheet from 'react-jss'
 
 import {H, V} from './utils/components/Flex.js'
 
-import {TopInsertable, RightInsertable} from './utils/components/Insertable.js'
+import {TopInsertable, LeftInsertable} from './utils/components/Insertable.js'
 
 class Design extends PureComponent {
   render() {
     return <div>
       <H>
-        <RightInsertable>
+        <LeftInsertable>
           <V>
             <TopInsertable>
               <div style={{
                 height : 30,
+                width : 100,
                 border : '1px solid gray',
-              }} >
-              这是个什么地方
-            </div>
-          </TopInsertable>
-        </V>
-      </RightInsertable>
-    </H>
-  </div>
+              }}>
+                aa
+              </div>
+            </TopInsertable>
+            <TopInsertable>
+              <div style={{
+                height : 30,
+                width : 100,
+                border : '1px solid gray',
+              }}>
+                bb
+              </div>
+            </TopInsertable>
+          </V>
+        </LeftInsertable>
+        <LeftInsertable>
+          <V>
+            <TopInsertable>
+              <div style={{
+                height : 30,
+                width : 100,
+                border : '1px solid gray',
+              }}>
+                aa
+              </div>
+            </TopInsertable>
+            <TopInsertable>
+              <div style={{
+                height : 30,
+                width : 100,
+                border : '1px solid gray',
+              }}>
+                bb
+              </div>
+            </TopInsertable>
+          </V>
+        </LeftInsertable>
+      </H>
+    </div>
   }
 }
 
