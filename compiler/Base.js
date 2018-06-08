5c801dcb3544d8281f433c6a1f75d970428856fe
insanehui <269200808@qq.com>
2018-06-08 19:20:26 +0800

    准备重写Line的逻辑

--- compiler/Base.js
+++ compiler/Base.js
@@ -6,4 +6,10 @@ export default class Base {
   toString = ()=>{
     return this.data
   }
+
+  /*
+   * 需要被子类覆盖。它的作用是，'吃'掉传入str里'属于'自己的部分，放入this.data中
+   * 然后返回str剩下的部分
+   */
+  read = str => str
 }
