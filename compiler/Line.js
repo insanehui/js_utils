1312da1f40dc168139bbdcec44948c4173c5abe7
insanehui <269200808@qq.com>
2018-06-08 19:27:14 +0800

    换为更简的正则

--- compiler/Line.js
+++ compiler/Line.js
@@ -2,11 +2,7 @@ import Base from './Base.js'
 
 export default class Line extends Base {
   read = str=>{
-    /*
-     * 这里有个陷阱，正好歪打正着：如果str里没有包含换行，则会consume掉整个字符串
-     * 因为下面的正则没有匹配到，所以str原样返回
-     */
-    const match = str.replace(/(^.*\n)[^]*/, '$1')
+    const match = /.*\n?/y.exec(str)[0]
     this.data = match
     return str.slice(match.length)
   }
