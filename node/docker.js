e3951ced8febd63b647417ec14b9c5f58e6daf21
insanehui <269200808@qq.com>
2018-07-14 18:08:29 +0800

    新搞一个docker.js

---
+++ node/docker.js
@@ -0,0 +1,38 @@
+/*
+ * 打镜像
+ */
+const fs = require('fs-extra')
+const path = require('path')
+const d = path.resolve.bind(null, __dirname)
+import moment from 'moment'
+import 'colors'
+
+import {cd, exec} from 'shelljs'
+import {need} from '../utils/node/cmd_line.js'
+import {tryExec} from '../utils/node/shell.js'
+
+function ensure_git_clean() {
+  const {stdout} = exec(`git status -s`)
+  need(!stdout, 'git not clean')
+}
+
+function build_img() {
+  exec_code(`docker build -t ${repo} .`)
+  const res = exec('docker images', {silent:true}).head({'-n' : 30})
+  console.log(res.toString())
+}
+
+function git_tag() {
+  exec_code(`git tag ${tag}`)
+}
+
+export function build(name){
+  const tag = moment().format('YYYYMMDD_HH.mm')
+  const repo = `${name}:${tag}`
+  console.log(repo.cyan)
+  cd(d('../../'))
+  ensure_git_clean()
+  build_img()
+  git_tag()
+  console.log(`================== img done ==================`.green)
+}
