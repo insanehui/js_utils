14f19ad10ccf4e492570a521e47c1b7eea23e51c
insanehui <269200808@qq.com>
2018-07-14 16:53:15 +0800

    promisify改名

--- node/child_process.js
+++ node/child_process.js
@@ -1,8 +1,3 @@
-/*
- * 增加一些经过promisify的原生库函数
- * TODO: 后续如果这里的函数多了，再按包名进行区分
- */
-// const util = require('util');
 const child_process = require('child_process')
 
 // export const exec = util.promisify(cp.exec); // 原来直接调用官方提供的promisify
