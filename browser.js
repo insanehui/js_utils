7ff8b8e3f8cd2a9c594299f0aac507e67f0607f6
insanehui <269200808@qq.com>
2018-07-27 17:48:07 +0800

    增加判断ie版本

--- browser.js
+++ browser.js
@@ -4,10 +4,28 @@
 
 export function is_ms(){ // 是否为微软系列的浏览器
   /*
-   * 依赖一些潜规则，在需要精准判断的场合不适合使用
+   * 这个办法区分不出来edge和ie
    */
   return !!navigator.msLaunchUri
 }
+
+// 取ie的版本。也能判断是否为ie
+function getIEVersion() {
+  var sAgent = window.navigator.userAgent;
+  var Idx = sAgent.indexOf("MSIE");
+
+  // If IE, return version number.
+  if (Idx > 0) 
+    return parseInt(sAgent.substring(Idx+ 5, sAgent.indexOf(".", Idx)), 10);
+
+  // If IE 11 then look for Updated user agent string.
+  else if (!!navigator.userAgent.match(/Trident\/7\./)) 
+    return 11;
+		
+  else
+    return 0; //It is not IE
+}
+export const IEVersion = getIEVersion()
 
 const userAgent = window.navigator.userAgent
 
