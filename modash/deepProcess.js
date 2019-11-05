c25fef3213d41e856fdaa8ec8db01572ed2f0ba2
insanehui <269200808@qq.com>
2019-11-05 20:16:33 +0800

    .

--- modash/deepProcess.js
+++ modash/deepProcess.js
@@ -4,18 +4,23 @@
 import deepMap from './deepMap.js'
 import lift from './funcOrderLift.js'
 
+/*
+ * 采用省略参数的写法，在参数处理上的逻辑就显得复杂很多
+ * 但使用起来就会方便一点
+ */
 export default function process(obj, fn, ...preds) {
   let pred = preds.pop()
   let through = false
-  if ( pred === true ) {
-    through = true
+  if ( pred === true || pred === false) {
+    through = pred
     pred = preds.pop()
   } 
+
   const func = through ? fn : lift(fn)
 
   if ( !preds.length ) {
     return deepMap(obj, func, pred)
   } 
   const high = v=>deepMap(v, func, pred)
-  return process(obj, high, ...preds)
+  return process(obj, high, ...preds, through)
 }
