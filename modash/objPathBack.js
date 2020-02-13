d0a1856593a781b49fca3bb3fcb27f0307bfa431
insanehui <269200808@qq.com>
2020-02-14 01:15:47 +0800

    完善一下情况

--- modash/objPathBack.js
+++ modash/objPathBack.js
@@ -4,6 +4,9 @@
 import _ from 'lodash'
 
 export default function back(g, keys, lvl = 1) {
+  if ( keys.length < lvl ) {
+    return null
+  } 
   keys = keys.slice(0, lvl ? -lvl : undefined)
   if (_.isEmpty(keys)) {
     return g
