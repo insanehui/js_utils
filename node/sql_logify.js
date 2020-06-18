a8a89156ae6b3de2a3c91a7df114fc840c67ad3a
insanehui <269200808@qq.com>
2020-06-18 18:33:38 +0800

    加了exist，但未测试

--- node/sql_logify.js
+++ node/sql_logify.js
@@ -9,11 +9,20 @@ const mysql = require('mysql')
  * db可能是promise-mysql的一个connection或者是pool
  */
 export function logify(db){ 
+
+  const query = (sql, para)=>{
+    sql = mysql.format(sql, para)
+    log(sql)
+    return db.query(sql) // 其实这是一个async函数，但由于这里没有出现await，故也不用加上async
+  }
+
+  const exist = async (table, obj) => {
+    const res = await query(`select 1 from ${table} where ?`, obj)
+    return !!res.length
+  }
+
   return {
-    query : (sql, para)=>{
-      sql = mysql.format(sql, para)
-      log(sql)
-      return db.query(sql) // 其实这是一个async函数，但由于这里没有出现await，故也不用加上async
-    }, 
+    exist,
+    query, 
   }
 }
