11f4494186b9e03ac3b84d43893bebd3af27efb5
insanehui <269200808@qq.com>
2020-10-12 00:16:58 +0800

    增加DataListInput

---
+++ components/Formy/DataListInput.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import _ from 'lodash'
+import uuid from 'uuid/v1'
+import {Input} from './adaptor.js'
+
+export default function DataListInput({list, as = Input, ...rest}) {
+  const As = as
+  const id = uuid()
+  return <>
+    <As list={id} {...rest}/>
+    <datalist id={id}>
+      {_.map(list, (item, i ) => <option key={i}>{item}</option>)}
+    </datalist>
+  </>
+}
+
