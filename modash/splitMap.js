8c21e788925801156151fd260e17cc790fb5bb45
insanehui <269200808@qq.com>
2021-02-09 12:36:22 +0800

    增加更详细的注释

--- modash/splitMap.js
+++ modash/splitMap.js
@@ -1,6 +1,8 @@
 /*
- * 一个类似string.split的map
+ * 一个类似string.split的map，区别在于分隔符是正则表达式reg
+ * head表示reg匹配后的字符串，tail表示从head后到下一个分隔符的内容
  * fn(head, tail)
+ * 因此如果原始串不是以匹配的分隔符开头的话，最前面一截内容将被丢弃
  */
 export default function splitMap(s, reg, fn = (head, tail)=>([head, tail])) {
   let p1 = 0, p2 = 0, p3
