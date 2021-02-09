aa4c58d0fd5d6ddfcc76a0052b260cd8a099de07
insanehui <269200808@qq.com>
2021-02-09 11:52:59 +0800

    注释了一下deepProcess

--- modash/deepProcess.js
+++ modash/deepProcess.js
@@ -10,17 +10,24 @@ import lift from './funcOrderLift.js'
  * 但使用起来就会方便一点
  */
 export default function process(obj, fn, ...preds) {
+
+  // 先将preds降一级
   let pred = preds.pop()
   let through = false
+
+  // 根据情况调整一下，因为可能preds可能包含了最后一个参数
   if ( pred === true || pred === false) {
     through = pred
     pred = preds.pop()
   } 
 
+  // 用deepMap吸纳pred，取代降维后的新fn（或者是proc）
   const proc = v=>deepMap(v, through ? fn : lift(fn), pred)
 
   if ( !preds.length ) {
-    return proc(obj)
+    return proc(obj) // 如果已经降维到尽头，直接返回
   } 
+
+  // 进入降维递归
   return process(obj, proc, ...preds, through)
 }
